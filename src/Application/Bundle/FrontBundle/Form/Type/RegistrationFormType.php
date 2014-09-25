c80fec4b75231c3851b12dec1edd6f5f0dc2af80
Nouman Tayyab <noumantayyab@gmail.com>
2014-09-25 16:13:52 +0500

    registration form

--- src/Application/Bundle/FrontBundle/Form/Type/RegistrationFormType.php
+++ src/Application/Bundle/FrontBundle/Form/Type/RegistrationFormType.php
@@ -13,14 +13,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         // add your custom field
-        $builder->add('name', 'text', array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Name')))
-            ->add('username', 'text', array('label' =>' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Username')))
-            ->add('email', 'email', array('label' => ' ', 'translation_domain' => 'FOSUserBundle', 'attr' => array('class' => 'form-control', 'placeholder' => 'Email')))
+        $builder->add('name', 'text', array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Name')))
+            ->add('username', 'text', array('label' =>'', 'attr' => array('class' => 'form-control', 'placeholder' => 'Username')))
+            ->add('email', 'email', array('label' => '', 'translation_domain' => 'FOSUserBundle', 'attr' => array('class' => 'form-control', 'placeholder' => 'Email')))
             ->add('plainPassword', 'repeated', array(
                 'type' => 'password',
                 'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Password')),
-                'second_options' => array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Confirm Password')),
+                'first_options' => array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Password')),
+                'second_options' => array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Confirm Password')),
                 'invalid_message' => 'fos_user.password.mismatch',
                 )
         );
