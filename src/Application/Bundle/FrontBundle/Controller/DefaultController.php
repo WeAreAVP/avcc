bf013f7166a9a847a4566f310092182273a12825
Nouman Tayyab <noumantayyab@gmail.com>
2014-09-28 15:55:34 +0500

    cs fixes using phpcs fixer of symfony 2

--- src/Application/Bundle/FrontBundle/Controller/DefaultController.php
+++ src/Application/Bundle/FrontBundle/Controller/DefaultController.php
@@ -3,12 +3,7 @@
 namespace Application\Bundle\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use FOS\UserBundle\FOSUserEvents;
-use FOS\UserBundle\Event\FormEvent;
-use FOS\UserBundle\Event\FilterUserResponseEvent;
-use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
@@ -21,17 +16,17 @@ class DefaultController extends Controller
     }
 
     /**
-     * 
+     *
      * @Template()
      */
     public function indexAction()
     {
         $user = $this->container->get('security.context')->getToken()->getUser();
-        if ( ! is_object($user) || ! $user instanceof UserInterface)
-        {
+        if ( ! is_object($user) || ! $user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
             $this->redirect($this->generateUrl("application_front"));
         }
+
         return $this->render('ApplicationFrontBundle:Default:index.html.twig', array('name' => 'me'));
     }
 
