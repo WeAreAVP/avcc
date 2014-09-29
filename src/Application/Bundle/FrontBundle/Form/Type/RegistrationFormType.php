ba9e3c06fdc9338d17bf4a6a81dc9208fccb24fd
Nouman Tayyab <noumantayyab@gmail.com>
2014-09-30 00:18:06 +0500

    minor changes + ran cs fixer

--- src/Application/Bundle/FrontBundle/Form/Type/RegistrationFormType.php
+++ src/Application/Bundle/FrontBundle/Form/Type/RegistrationFormType.php
@@ -13,17 +13,7 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         // add your custom field
-        $builder->add('name', 'text', array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Name')))
-            ->add('username', 'text', array('label' =>'', 'attr' => array('class' => 'form-control', 'placeholder' => 'Username')))
-            ->add('email', 'email', array('label' => '', 'translation_domain' => 'FOSUserBundle', 'attr' => array('class' => 'form-control', 'placeholder' => 'Email')))
-            ->add('plainPassword', 'repeated', array(
-                'type' => 'password',
-                'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Password'),'label_attr' => array('style' => 'visibility:hidden;display:none')),
-                'second_options' => array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Confirm Password'),'label_attr' => array('style' => 'visibility:hidden;display:none')),
-                'invalid_message' => 'fos_user.password.mismatch',
-                )
-        );
+        $builder->add('name');
     }
 
     public function getParent()
