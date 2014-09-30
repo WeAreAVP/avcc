31d14fbbc084036b89fb5082a9546b5d713f8a54
Nouman Tayyab <noumantayyab@gmail.com>
2014-09-30 15:57:09 +0500

    routing issue

--- src/Application/Bundle/FrontBundle/Controller/DefaultController.php
+++ src/Application/Bundle/FrontBundle/Controller/DefaultController.php
@@ -49,7 +49,8 @@ class DefaultController extends Controller
 
         if ($error) {
             // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
-            $error = $error->getMessage();
+//            $error = $error->getMessage();
+            $error = "Invalid username or password";
         }
         // last username entered by the user
         $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
