724c47b67c6e2d8de9a934e3de92af5de301fe0a
Nouman Tayyab <noumantayyab@gmail.com>
2014-09-30 14:12:29 +0500

    login issue

--- src/Application/Bundle/FrontBundle/Form/Type/ProfileFormType.php
+++ src/Application/Bundle/FrontBundle/Form/Type/ProfileFormType.php
@@ -9,6 +9,12 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class ProfileFormType extends AbstractType
 {
+   /**
+    * Form overrided to show name field
+    * 
+    * @param \Symfony\Component\Form\FormBuilderInterface $builder
+    * @param array $options
+    */  
    public function buildForm(FormBuilderInterface $builder, array $options)
     {
        parent::buildForm($builder, $options);
@@ -16,12 +22,22 @@ class ProfileFormType extends AbstractType
             ->add('name', 'text', array('label' => 'form.name', 'translation_domain' => 'FOSUserBundle'))
         ;
     }
-
+    
+   /**
+    * Returns parent form type
+    * 
+    * @return string
+    */
     public function getParent()
     {
         return 'fos_user_profile';
     }
     
+    /**
+     * Returns new form type 
+     * 
+     * @return string
+     */
     public function getName()
     {
         return 'application_user_profile';
