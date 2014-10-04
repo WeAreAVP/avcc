c88bab07bf5aed2aa9929702e5870a3002c50878
Nouman Tayyab <noumantayyab@gmail.com>
2014-10-04 15:13:43 +0500

    code formation for build

--- src/Application/Bundle/FrontBundle/Controller/DefaultController.php
+++ src/Application/Bundle/FrontBundle/Controller/DefaultController.php
@@ -9,48 +9,69 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\SecurityContext;
 
-
+/**
+ * Log In
+ * 
+ */
 class DefaultController extends Controller
 {
 
+    /**
+     * calling parent bundle
+     * 
+     * @return string
+     */
     public function getParent()
     {
         return 'FOSUserBundle';
     }
 
     /**
-     *
      * @Template()
      */
     public function indexAction()
     {
         $user = $this->container->get('security.context')->getToken()->getUser();
-        if ( ! is_object($user) || ! $user instanceof UserInterface) {
+        if ( ! is_object($user) || ! $user instanceof UserInterface)
+        {
             throw new AccessDeniedException('This user does not have access to this section.');
             $this->redirect($this->generateUrl("application_front"));
         }
 
         return $this->render('ApplicationFrontBundle:Default:index.html.twig', array('name' => $user->getUsername()));
     }
-   
+
+    /**
+     *  
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * 
+     * @return type
+     */
     public function loginAction(Request $request)
     {
         /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
         $session = $request->getSession();
         // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+        {
             $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+        }
+        elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR))
+        {
             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
             $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
+        }
+        else
+        {
             $error = '';
         }
 
-        if ($error) {
+        if ($error)
+        {
             // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
             $error = $error->getMessage();
-            if(strtolower($error)==='bad credentials'){
+            if (strtolower($error) === 'bad credentials')
+            {
                 $error = "Invalid username or password.";
             }
 //            $error = "Invalid username or password";
@@ -58,17 +79,15 @@ class DefaultController extends Controller
         // last username entered by the user
         $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
 
-        $csrfToken = $this->container->has('form.csrf_provider')
-            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
-            : null;
+        $csrfToken = $this->container->has('form.csrf_provider') ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate') : null;
 
         return $this->renderLogin(array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-            'csrf_token' => $csrfToken,
+                'last_username' => $lastUsername,
+                'error' => $error,
+                'csrf_token' => $csrfToken,
         ));
     }
-    
+
     /**
      * Renders the login template with the given parameters. Overwrite this function in
      * an extended controller to provide additional data for the login template.
@@ -83,7 +102,11 @@ class DefaultController extends Controller
 
         return $this->container->get('templating')->renderResponse($template, $data);
     }
-
+    
+    /**
+     * 
+     * @throws \RuntimeException
+     */
     public function checkAction()
     {
         throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
