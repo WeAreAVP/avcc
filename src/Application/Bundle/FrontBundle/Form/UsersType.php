82e010b7d1998c6ecb1b9d32718a6761bab8927d
Nouman Tayyab <noumantayyab@gmail.com>
2014-10-04 14:13:48 +0500

    chosen applied on organizations and roles fields in user module

--- src/Application/Bundle/FrontBundle/Form/UsersType.php
+++ src/Application/Bundle/FrontBundle/Form/UsersType.php
@@ -12,68 +12,66 @@ use Application\Bundle\FrontBundle\Form\DataTransformer\StringToArrayTransformer
 class UsersType extends AbstractType
 {
 
-	public $roles;
-	
+    public $roles;
 
-	public function __construct($options = array())
-	{
-		$this->roles = $options['roles'];
-		
-	}
+    public function __construct($options = array())
+    {
+        $this->roles = $options['roles'];
+    }
 
-	/**
-	 * @param FormBuilderInterface $builder
-	 * @param array $options
-	 */
-	public function buildForm(FormBuilderInterface $builder, array $options)
-	{
-		if (class_exists('Symfony\Component\Security\Core\Validator\Constraints\UserPassword'))
-		{
-			$constraint = new UserPassword();
-		}
-		else
-		{
-			// Symfony 2.1 support with the old constraint class
-			$constraint = new OldUserPassword();
-		}
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (class_exists('Symfony\Component\Security\Core\Validator\Constraints\UserPassword'))
+        {
+            $constraint = new UserPassword();
+        }
+        else
+        {
+            // Symfony 2.1 support with the old constraint class
+            $constraint = new OldUserPassword();
+        }
 
-		$transformer = new StringToArrayTransformer();
-		$builder
-		->add('name')
-		->add('username')
-		->add('email')
-		->add('organizations')
-		->add('plainPassword', 'repeated', array(
-			'type' => 'password',
-			'required' => false,
-			'options' => array('translation_domain' => 'FOSUserBundle'),
-			'first_options' => array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Password')),
-			'second_options' => array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Confirm Password')),
-			'invalid_message' => 'fos_user.password.mismatch',
-		)
-		)
-		->add('roles', 'choice', array(
-			'choices' => $this->roles,
-			'multiple' => true,
-		));
-	}
+        $transformer = new StringToArrayTransformer();
+        $builder
+            ->add('name')
+            ->add('username')
+            ->add('email')
+            ->add('organizations')
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'required' => false,
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Password')),
+                'second_options' => array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Confirm Password')),
+                'invalid_message' => 'fos_user.password.mismatch',
+                )
+            )
+            ->add('roles', 'choice', array(
+                'choices' => $this->roles,
+                'multiple' => true,
+        ));
+    }
 
-	/**
-	 * @param OptionsResolverInterface $resolver
-	 */
-	public function setDefaultOptions(OptionsResolverInterface $resolver)
-	{
-		$resolver->setDefaults(array(
-			'data_class' => 'Application\Bundle\FrontBundle\Entity\Users'
-		));
-	}
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Application\Bundle\FrontBundle\Entity\Users'
+        ));
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getName()
-	{
-		return 'application_bundle_frontbundle_users';
-	}
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'application_bundle_frontbundle_users';
+    }
 
 }
---
+++ src/Application/Bundle/FrontBundle/Resources/public/js/users.js
@@ -0,0 +1,4 @@
+function initialize_form() {
+    $('#application_bundle_frontbundle_users_organizations').chosen();
+    $('#application_bundle_frontbundle_users_roles').chosen({max_selected_options: 1});
+}
