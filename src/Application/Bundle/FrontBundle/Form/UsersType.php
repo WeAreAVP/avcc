49e9f4ef19eb346dfdc23275c772b6aaa6b7f124
Nouman Tayyab <noumantayyab@gmail.com>
2014-10-07 01:22:45 +0500

    added metro ui and did design changes according to new metro ui

--- src/Application/Bundle/FrontBundle/Form/UsersType.php
+++ src/Application/Bundle/FrontBundle/Form/UsersType.php
@@ -7,17 +7,19 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Security\Core\Validator\Constraint\UserPassword as OldUserPassword;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class UsersType extends AbstractType
 {
 
     public $roles;
-    public $user_role;
+    public $userRole;
 
     public function __construct($options = array())
     {
         $this->roles = $options['roles'];
-        $this->user_role = isset($options['user_role']) ? $options['user_role'] : null;
+        $this->userRole = isset($options['user_role']) ? $options['user_role'] : null;
     }
 
     /**
@@ -29,36 +31,54 @@ class UsersType extends AbstractType
         if (class_exists('Symfony\Component\Security\Core\Validator\Constraints\UserPassword')) {
             $constraint = new UserPassword();
         } else {
-            // Symfony 2.1 support with the old constraint class
             $constraint = new OldUserPassword();
         }
         $data = array();
         $required = true;
-        if (is_array($this->user_role) && !$this->user_role) {
-            $data = array('ROLE_USER');
-            $required = FALSE;
-        } else {
-            $data = $this->user_role;
-        }
+        $role = 'ROLE_USER';
+        if (count($this->userRole) > 0)
+            $role = $this->userRole[0];
+
         $builder
-                ->add('name')
-                ->add('username')
-                ->add('email')
-                ->add('organizations')
-                ->add('plainPassword', 'repeated', array(
-                    'type' => 'password',
-                    'required' => $required,
-                    'options' => array('translation_domain' => 'FOSUserBundle'),
-                    'first_options' => array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Password')),
-                    'second_options' => array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Confirm Password')),
-                    'invalid_message' => 'fos_user.password.mismatch',
-                        )
-                )
-                ->add('roles', 'choice', array(
-                    'choices' => $this->roles,
-                    'multiple' => true,
-                    'data' => $data
+        ->add('name')
+        ->add('username')
+        ->add('email')
+        ->add('organizations')
+        ->add('plainPassword', 'repeated', array(
+            'type' => 'password',
+            'required' => $required,
+            'options' => array('translation_domain' => 'FOSUserBundle'),
+            'first_options' => array('label' => '', 'attr' => array('class' => 'form-control', 'placeholder' => 'Password')),
+            'second_options' => array('label' => ' ', 'attr' => array('class' => 'form-control', 'placeholder' => 'Confirm Password')),
+            'invalid_message' => 'fos_user.password.mismatch',
+        )
+        )
+        ->add('roles', 'choice', array(
+            'choices' => $this->roles,
+            'multiple' => false,
+            'data' => $role
+        ))
+        ->addEventListener(
+        FormEvents::PRE_SUBMIT, array($this, 'onPreSubmitData'));
+    }
+
+    /**
+     * Add or remove field before validation.
+     * Change value before binding.
+     *
+     * @param FormEvent $event
+     */
+    public function onPreSubmitData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $userInfo = $event->getData();
+        $form->remove('roles');
+        $form->add('roles', 'choice', array(
+            'choices' => $this->roles,
+            'multiple' => true,
         ));
+        $userInfo['roles'] = array($userInfo['roles']);
+        $event->setData($userInfo);
     }
 
     /**
--- src/Application/Bundle/FrontBundle/Resources/public/js/users.js
+++ src/Application/Bundle/FrontBundle/Resources/public/js/users.js
@@ -1,4 +1,4 @@
 function initialize_form() {
     $('#application_bundle_frontbundle_users_organizations').chosen();
-    $('#application_bundle_frontbundle_users_roles').chosen({max_selected_options: 1});
+    $('#application_bundle_frontbundle_users_roles').chosen();
 }
