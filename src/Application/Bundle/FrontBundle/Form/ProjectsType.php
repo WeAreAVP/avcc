aadd89e703a3371e22cd239886183a8d5bb045f0
Rubab Rafique <rubabrafique@Rubabs-Mac-mini.local>
2014-10-13 13:30:37 +0500

    task avcc-23 - remove organizations field if user is not super admin

--- src/Application/Bundle/FrontBundle/Form/ProjectsType.php
+++ src/Application/Bundle/FrontBundle/Form/ProjectsType.php
@@ -15,12 +15,36 @@ class ProjectsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('organization')
-
+            ->add('name')            
+            ->addEventListener(
+                        FormEvents::POST_SET_DATA, array($this, 'onPreSetData'))     
+            ->addEventListener(
+                        FormEvents::POST_SUBMIT, array($this, 'onPostSubmitData'));    
         ;
     }
+    
+        public function onPreSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+       
+        $loggedInUserRole = $this->user->getRoles();
 
+        if ($loggedInUserRole[self::$DEFAULT_ROLE_INDEX] == self::$DEFAULT_SUPER_ADMIN_ROLE) {
+            $form->add('organizations');
+        } 
+    }
+    
+    public function onPostSubmitData(FormEvent $event)
+    {
+        $projectInfo = $event->getData();
+
+        $loggedInUserRole = $this->user->getRoles();
+        if ($loggedInUserRole[self::$DEFAULT_ROLE_INDEX] !== self::$DEFAULT_SUPER_ADMIN_ROLE) {
+
+            if ($this->user->getOrganizations() instanceof \Application\Bundle\FrontBundle\Entity\Organizations)
+                $projectInfo->setOrganizations($this->user->getOrganizations());
+        }
+    }
     /**
      * @param OptionsResolverInterface $resolver
      */
