ff0ac48a647db2af677fd125504a2089809e8e55
Nouman <nouman@avpreserve.com>
2014-10-21 13:26:09 +0500

    php fixer run

--- src/Application/Bundle/FrontBundle/Form/ReelDiametersType.php
+++ src/Application/Bundle/FrontBundle/Form/ReelDiametersType.php
@@ -10,7 +10,7 @@ class ReelDiametersType extends AbstractType
 {
         /**
      * @param FormBuilderInterface $builder
-     * @param array $options
+     * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -20,7 +20,7 @@ class ReelDiametersType extends AbstractType
 //            ->add('organization')
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
