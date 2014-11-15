d26de6749349ad7ee33dca05b2026e2e2e9250de
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-16 00:09:08 +0500

    debugging for sphinx insertion

--- src/Application/Bundle/FrontBundle/Form/AudioRecordsType.php
+++ src/Application/Bundle/FrontBundle/Form/AudioRecordsType.php
@@ -10,58 +10,54 @@ use Doctrine\ORM\EntityManager;
 class AudioRecordsType extends AbstractType
 {
 
-    private $data;
-    private $em;
-    private $sphinxParam;
+	private $data;
+	private $em;
+	private $sphinxParam;
 
-    public function __construct(EntityManager $em, $data = null, $sphinxParam = null)
-    {
-        $this->data = $data;
-        $this->em = $em;
-        $this->sphinxParam = $sphinxParam;
-    }
+	public function __construct(EntityManager $em, $data = null, $sphinxParam = null)
+	{
+		$this->data = $data;
+		$this->em = $em;
+		$this->sphinxParam = $sphinxParam;
+	}
 
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-                ->add('record', new RecordsType($this->em, $this->data, $this->sphinxParam), array(
-                    'data_class' => 'Application\Bundle\FrontBundle\Entity\Records'))
-                ->add('mediaDuration', 'text', array('required' => false))
-                ->add('diskDiameters')
-                ->add('mediaDiameters')
-                ->add('bases')
-                ->add('recordingSpeed')
-                ->add('tapeThickness')
-                ->add('slides')
-                ->add('trackTypes')
-                ->add('monoStereo')
-                ->add('noiceReduction')
-//		->add('nextStep', 'submit')
-//    ->add('previousStep', 'submit')
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array                $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+		->add('record', new RecordsType($this->em, $this->data, $this->sphinxParam), array(
+			'data_class' => 'Application\Bundle\FrontBundle\Entity\Records'))
+		->add('mediaDuration', 'text', array('required' => false))
+		->add('diskDiameters')
+		->add('mediaDiameters')
+		->add('bases')
+		->add('recordingSpeed')
+		->add('tapeThickness')
+		->add('slides')
+		->add('trackTypes')
+		->add('monoStereo')
+		->add('noiceReduction');
+	}
 
-        ;
-    }
+	/**
+	 * @param OptionsResolverInterface $resolver
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(array(
+			'data_class' => 'Application\Bundle\FrontBundle\Entity\AudioRecords'
+		));
+	}
 
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'Application\Bundle\FrontBundle\Entity\AudioRecords'
-        ));
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'application_bundle_frontbundle_audiorecords';
-    }
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return 'application_bundle_frontbundle_audiorecords';
+	}
 
 }
