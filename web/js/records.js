0bc166367e2c5f13cd22cd58f99dbe9d85219e1e
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-17 00:58:14 +0500

    working on datatables

--- web/js/records.js
+++ web/js/records.js
@@ -1,68 +1,36 @@
-/**
- * Class of managing Filtering and displaying of records.
- * 
- * @returns {Records}
- */
-function Records() {
-	/**
-	 * Object of datatable.
-	 */
-//	this.oTable = null;
-	/**
-	 * URL for managing calls of datatable
-	 * @type {string}
-	 */
-	var ajaxSource = null;
-	/**
-	 * Set the ajax URL of datatable.
-	 * @param {string} source
-	 * 
-	 */
-	this.setAjaxSource = function (source) {
-		ajaxSource = source;
+var oTable = null;
 
-	}
-	/**
-	 * Initialize the datatable.
-	 * 
-	 */
-	this.initDataTable = function () {
-		// check the existence of table on which we are going to apply datatable.
-		if ($('#records').length > 0)
+function initDataTable(ajaxSource) {
+	// check the existence of table on which we are going to apply datatable.
+	if ($('#records').length > 0)
+	{
+		// Modify css for managing view.
+		$('#container').removeClass('container');
+		$('#container').css('margin', '20px');
+		oTable =
+		$('#records').dataTable(
 		{
-			// Modify css for managing view.
-			$('#container').removeClass('container');
-			$('#container').css('margin', '20px');
-			oTable =
-			$('#records').dataTable(
-			{
-				"dom": '<"top"p><"clear">tir<"bottom"p>',
-				"bProcessing": true,
-				"bServerSide": true,
-				"language": {
-					"info": "Showing _START_ - _END_ of _MAX_"
-				},
-				"aoColumnDefs": [
-					{"bSortable": false, "aTargets": [0]}
-				],
-				"aaSorting": [],
-				"sAjaxSource": ajaxSource,
-				"bStateSave": true,
-				"fnInitComplete": function () {
-					this.oTable.fnAdjustColumnSizing();
-				},
-				"fnServerData": function (sSource, aoData, fnCallback) {
-					jQuery.getJSON(sSource, aoData, function (json) {
-						fnCallback(json);
+			"dom": '<"top"p><"clear">tir<"bottom"p>',
+			"bProcessing": true,
+			"bServerSide": true,
+			"language": {
+				"info": "Showing _START_ - _END_ of _MAX_"
+			},
+			"aoColumnDefs": [
+				{"bSortable": false, "aTargets": [0]}
+			],
+			"aaSorting": [],
+			"sAjaxSource": ajaxSource,
+			"bStateSave": true,
+			"fnInitComplete": function () {
+				oTable.fnAdjustColumnSizing();
+			},
+			"fnServerData": function (sSource, aoData, fnCallback) {
+				jQuery.getJSON(sSource, aoData, function (json) {
+					fnCallback(json);
 
-					});
-				},
-			});
-		}
-
+				});
+			},
+		});
 	}
 }
-
-
-
-
