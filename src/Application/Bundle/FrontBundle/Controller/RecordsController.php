ab427ce2530e3419d73fadb2561d7163ef8ac7ac
Nouman <nouman@avpreserve.com>
2014-11-18 13:08:59 +0500

    facets

--- src/Application/Bundle/FrontBundle/Controller/RecordsController.php
+++ src/Application/Bundle/FrontBundle/Controller/RecordsController.php
@@ -21,100 +21,113 @@ use Application\Bundle\FrontBundle\SphinxSearch\SphinxSearch;
 class RecordsController extends Controller
 {
 
-	private $columnOrder = array();
-	private $defaultFields;
-	private $columns = array();
-	private $userFields = array();
-	private $session;
-	private $offset;
-	private $limit;
+    private $columnOrder = array();
+    private $defaultFields;
+    private $columns = array();
+    private $userFields = array();
+    private $session;
+    private $offset;
+    private $limit;
 
-	public function __construct()
-	{
-		$this->columns = array(
-			'checkbox_Col' => 'checkboxCol',
-			'Project_Name' => 'projectName',
-			'Unique_ID' => 'uniqueId',
-			'Title' => 'title',
-			'Collection_Name' => 'collectionName',
-			'Location' => 'location'
-		);
-		;
-		$this->defaultFields = new DefaultFields();
-		$this->limit = 10;
-	}
+    public function __construct()
+    {
+        $this->columns = array(
+            'checkbox_Col' => 'checkboxCol',
+            'Project_Name' => 'projectName',
+            'Unique_ID' => 'uniqueId',
+            'Title' => 'title',
+            'Collection_Name' => 'collectionName',
+            'Location' => 'location'
+        );
+        ;
+        $this->defaultFields = new DefaultFields();
+        $this->limit = 10;
+    }
 
-	/**
-	 * Lists all AudioRecords entities.
-	 *
-	 * @Route("/", name="record_list")
-	 * @Method("GET")
-	 * @Template()
-	 * @return array
-	 */
-	public function indexAction(Request $request)
-	{
-		$em = $this->getDoctrine()->getManager();
-		$sphinxSearch = new SphinxSearch($em);
-		$facet['mediaType'] = $sphinxSearch->facetSelect('media_type');
-		
-		return array(
-			'facets' => $facet,
-			'columns' => $this->columns
-		);
-	}
+    /**
+     * Lists all AudioRecords entities.
+     *
+     * @Route("/", name="record_list")
+     * @Method("GET")
+     * @Template()
+     * @return array
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $sphinxSearch = new SphinxSearch($em);
+        $facet['mediaType'] = $sphinxSearch->facetSelect('media_type');
 
-	/**
-	 * Make records to display for dataTables.
-	 * 
-	 * @param Request $request 
-	 * 
-	 * @Route("/dataTable", name="record_dataTable")
-	 * @Method("GET")
-	 * @Template("ApplicationFrontBundle:AudioRecords:dataTable.html.php") 
-	 * @return json
-	 */
-	public function dataTableAction(Request $request)
-	{
-		$columns = array(0 => '',
-			1 => 'title',
-			2 => 'unique_id',
-			3 => 'title',
-			4 => 'collection_name',
-			5 => 'location'
-		);
-		$em = $this->getDoctrine()->getManager();
-		$sEcho = $request->query->get('sEcho');
-		$sortOrder = $request->query->get('sSortDir_0') ? $request->query->get('sSortDir_0') : 'asc';
-		$sortIndex = $request->query->get('iSortCol_0') ? $columns[$request->query->get('iSortCol_0')] : 'title';
+        return array(
+            'facets' => $facet,
+            'columns' => $this->columns
+        );
+    }
 
-
-		$offset = $request->query->get('iDisplayStart') ? $request->query->get('iDisplayStart') : 0;
-		$limit = $request->query->get('iDisplayLength') ? $request->query->get('iDisplayLength') : 10;
+    /**
+     * Make records to display for dataTables.
+     * 
+     * @param Request $request 
+     * 
+     * @Route("/dataTable", name="record_dataTable")
+     * @Method("GET")
+     * @Template("ApplicationFrontBundle:AudioRecords:dataTable.html.php") 
+     * @return json
+     */
+    public function dataTableAction(Request $request)
+    {
+        $columns = array(0 => '',
+            1 => 'title',
+            2 => 'unique_id',
+            3 => 'title',
+            4 => 'collection_name',
+            5 => 'location'
+        );
+        $em = $this->getDoctrine()->getManager();
+        $sEcho = $request->query->get('sEcho');
+        $sortOrder = $request->query->get('sSortDir_0') ? $request->query->get('sSortDir_0') : 'asc';
+        $sortIndex = $request->query->get('iSortCol_0') ? $columns[$request->query->get('iSortCol_0')] : 'title';
 
 
-		$sphinxSearch = new SphinxSearch($em);
-		$result = $sphinxSearch->select($offset, $limit, $sortIndex, $sortOrder);
-		$records = $result[0];
-		$currentPageTotal = count($records);
-		$totalRecords = $result[1][0]['Value'];
+        $offset = $request->query->get('iDisplayStart') ? $request->query->get('iDisplayStart') : 0;
+        $limit = $request->query->get('iDisplayLength') ? $request->query->get('iDisplayLength') : 10;
 
 
-		$tableView = $this->defaultFields->recordDatatableView($records);
+        $sphinxSearch = new SphinxSearch($em);
+        $result = $sphinxSearch->select($offset, $limit, $sortIndex, $sortOrder);
+        $records = $result[0];
+        $currentPageTotal = count($records);
+        $totalRecords = $result[1][0]['Value'];
 
-		$dataTable = array(
-			'sEcho' => intval($sEcho),
-			'iTotalRecords' => intval($currentPageTotal),
-			'iTotalDisplayRecords' => intval($totalRecords),
-			'aaData' => $tableView
-		);
-		echo json_encode($dataTable);
-		exit;
-	}
 
-	protected function getSphinxInfo()
-	{
-		return $this->container->getParameter('sphinx_param');
-	}
+        $tableView = $this->defaultFields->recordDatatableView($records);
+
+        $dataTable = array(
+            'sEcho' => intval($sEcho),
+            'iTotalRecords' => intval($currentPageTotal),
+            'iTotalDisplayRecords' => intval($totalRecords),
+            'aaData' => $tableView
+        );
+        echo json_encode($dataTable);
+        exit;
+    }
+
+    protected function getSphinxInfo()
+    {
+        return $this->container->getParameter('sphinx_param');
+    }
+    
+    /**
+     * 
+     * @param Request $request
+     * 
+     * @Route("/facets", name="record_facets")
+     * @Method("POST")
+     */
+    public function facetsAction(Request $request)
+    {
+       print_r($request->request->get('formSearch'));
+       exit;
+    }
 
 }
--- web/js/records.js
+++ web/js/records.js
@@ -4,113 +4,132 @@
  * @returns {Records}
  */
 function Records() {
-	/**
-	 * Object of datatable.
-	 */
+    /**
+     * Object of datatable.
+     */
 //	this.oTable = null;
-	/**
-	 * URL for managing calls of datatable
-	 * @type {string}
-	 */
-	var ajaxSource = null;
-	/**
-	 * Set the ajax URL of datatable.
-	 * @param {string} source
-	 * 
-	 */
-	this.setAjaxSource = function (source) {
-		ajaxSource = source;
+    /**
+     * URL for managing calls of datatable
+     * @type {string}
+     */
+    var ajaxSource = null;
+    var pageUrl = null;
+    /**
+     * Set the ajax URL of datatable.
+     * @param {string} source
+     * 
+     */
+    this.setAjaxSource = function (source) {
+        ajaxSource = source;
 
-	}
-	/**
-	 * Initialize the datatable.
-	 * 
-	 */
-	this.initDataTable = function () {
-		// check the existence of table on which we are going to apply datatable.
-		if ($('#records').length > 0)
-		{
-			// Modify css for managing view.
-			$('#container').removeClass('container');
-			$('#container').css('margin', '20px');
-			oTable =
-			$('#records').dataTable(
-			{
-				"dom": '<"top"p><"clear">tir<"bottom"p>',
-				"bProcessing": true,
-				"bServerSide": true,
-				retrieve: true,
-				destroy: true,
-				"language": {
-					"info": "Showing _START_ - _END_ of _MAX_",
-					"infoFiltered": ''
-				},
-				"aoColumnDefs": [
-					{"bSortable": false, "aTargets": [0]}
-				],
-				"aaSorting": [],
-				"sAjaxSource": ajaxSource,
-				"bStateSave": true,
+    }
+    /**
+     * Set the page url.
+     * @param {string} url
+     * 
+     */
+    this.setPageUrl = function (url) {
+        pageUrl = url;
+
+    }
+    /**
+     * Initialize the datatable.
+     * 
+     */
+    this.initDataTable = function () {
+        // check the existence of table on which we are going to apply datatable.
+        if ($('#records').length > 0)
+        {
+            // Modify css for managing view.
+            $('#container').removeClass('container');
+            $('#container').css('margin', '20px');
+            oTable =
+                    $('#records').dataTable(
+                    {
+                        "dom": '<"top"p><"clear">tir<"bottom"p>',
+                        "bProcessing": true,
+                        "bServerSide": true,
+                        retrieve: true,
+                        destroy: true,
+                        "language": {
+                            "info": "Showing _START_ - _END_ of _MAX_",
+                            "infoFiltered": ''
+                        },
+                        "aoColumnDefs": [
+                            {"bSortable": false, "aTargets": [0]}
+                        ],
+                        "aaSorting": [],
+                        "sAjaxSource": ajaxSource,
+                        "bStateSave": true,
 //				"fnInitComplete": function () {
 //					this.oTable.fnAdjustColumnSizing();
 //				},
-				"fnServerData": function (sSource, aoData, fnCallback) {
-					jQuery.getJSON(sSource, aoData, function (json) {
-						fnCallback(json);
+                        "fnServerData": function (sSource, aoData, fnCallback) {
+                            jQuery.getJSON(sSource, aoData, function (json) {
+                                fnCallback(json);
 
-					});
-				},
-			});
-		}
+                            });
+                        },
+                    });
+        }
 
-	}
-	/**
-	 * 
-	 * @returns {Boolean}
-	 */
-	this.bindEvents = function () {
-		$('input[name="mediaType[]"]').click(function () {
-			checkParentFacet('media_type', $(this).attr('checked'));
-		});
-		return true;
-	}
-	/**
-	 * 
-	 * @param {type} type
-	 * @param {type} isChecked
-	 * @returns {undefined}
-	 */
-	var checkParentFacet = function (type, isChecked) {
-		//todo need to update function
-		var totalChecked = $('#total_checked').val();
-		var total = $('#facet_sidebar input:checked').length;
-		if (total == 0)
-			totalChecked = 0;
-		if (isChecked == 'checked')
-			totalChecked++;
-		else
-			totalChecked--;
+    }
+    /**
+     * 
+     * @returns {Boolean}
+     */
+    this.bindEvents = function () {
+        $('input[name="mediaType[]"]').click(function () {
+            checkParentFacet('media_type', $(this).attr('checked'));
+        });
+        return true;
+    }
+    /**
+     * 
+     * @param {type} type
+     * @param {type} isChecked
+     * @returns {undefined}
+     */
+    var checkParentFacet = function (type, isChecked) {
+        //todo need to update function
+        var totalChecked = $('#total_checked').val();
+        var total = $('#facet_sidebar input:checked').length;
+        if (total == 0)
+            totalChecked = 0;
+        if (isChecked == 'checked')
+            totalChecked++;
+        else
+            totalChecked--;
 
-		$('#total_checked').val(totalChecked);
+        $('#total_checked').val(totalChecked);
 
-		if ($('#parent_facet').val() == '' && totalChecked == 1)
-			$('#parent_facet').val(type);
-		else if (totalChecked == 0)
-			$('#parent_facet').val('');
-		filterRecords();
-	}
-	/**
-	 * 
-	 * @returns {undefined}
-	 */
-	var filterRecords = function () {
+        if ($('#parent_facet').val() == '' && totalChecked == 1)
+            $('#parent_facet').val(type);
+        else if (totalChecked == 0)
+            $('#parent_facet').val('');
+        filterRecords();
+    }
+    /**
+     * 
+     * @returns {undefined}
+     */
+    var filterRecords = function () {
+        $.ajax({
+            type: 'POST',
+            url: pageUrl,
+            data: $('#formSearch').serialize(),
+            dataType: 'json',
+            success: function (response)
+            {
+                console.log(response);
+            }
 
-		//send ajax call here.
+        });
 
 
 
 
-	}
+    }
 }
 
 
