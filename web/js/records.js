a3513d610609e1e46061749bf9a0c9de19c4215f
Nouman <nouman@avpreserve.com>
2014-11-19 16:42:37 +0500

    facets

--- web/js/records.js
+++ web/js/records.js
@@ -164,7 +164,7 @@ function Records() {
      */
     var filterRecords = function () {
         $.ajax({
-            type: 'POST',
+            type: 'GET',
             url: pageUrl,
             data: $('#formSearch').serialize(),
             dataType: 'json',
