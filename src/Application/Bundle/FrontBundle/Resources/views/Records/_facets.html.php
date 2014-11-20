9d087b2d828aa443203561f23e13bf5493b66adb
Nouman <nouman@avpreserve.com>
2014-11-20 15:46:28 +0500

    keywords

--- src/Application/Bundle/FrontBundle/Resources/views/Records/_facets.html.php
+++ src/Application/Bundle/FrontBundle/Resources/views/Records/_facets.html.php
@@ -13,9 +13,9 @@
                         <input type="text" id="keywordSearch" value="" />
                     </div>
                     <div class="button-dropdown">
-                        <a href="javascript://;" class="button dropdown-toggle">
+                        <div class="button dropdown-toggle">
                            <span id="limit_field_text">Search</span>
-                        </a>
+                        </div>
                         <ul class="dropdown-menu" data-role="dropdown">
                            <li><a href="javascript://;" class="customToken" data-fieldName="Title"  data-columnName="title" style="font-size: 14px!important;">Title</a></li>
                            <li><a href="javascript://;" class="customToken" data-fieldName="Description"  data-columnName="description" style="font-size: 14px!important;">Description</a></li>
