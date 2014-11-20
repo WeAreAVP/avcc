7cb9baa484745ecd9f603f37e4e84bce566f864c
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-20 12:13:37 +0500

    worked on reporting task

--- src/Application/Bundle/FrontBundle/Controller/ReportController.php
+++ src/Application/Bundle/FrontBundle/Controller/ReportController.php
@@ -8,8 +8,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-
-
 /**
  * ReelDiameters controller.
  *
@@ -17,6 +15,54 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class ReportController extends Controller
 {
+
+	private $columns = array(
+		'Project_Name',
+		'Collection_Name',
+		'Media_Type',
+		'Unique_ID',
+		'Location',
+		'Format',
+		'Title',
+		'Description',
+		'Commercial_or_Unique',
+		'Content_Duration',
+		'Media_Duration',
+		'Creation_Date',
+		'Content_Date',
+		'Base',
+		'Print_Type',
+		'Disk_Diameter',
+		'Reel_Diameter',
+		'Media_Diameter',
+		'Footage',
+		'Recording_Speed',
+		'Color',
+		'Tape_Thickness',
+		'Sides',
+		'Track_Type',
+		'Mono_or_Stereo',
+		'Noise_Reduction',
+		'Cassette_Size',
+		'Format_Version',
+		'Recording_Standard',
+		'Reel_or_Core',
+		'Sound',
+		'Frame_Rate',
+		'Acid_Detection_Strip',
+		'Shrinkage',
+		'Genre_Terms',
+		'Contributor',
+		'Generation',
+		'Part',
+		'Copyright_/_Restrictions',
+		'Duplicates_/_Derivatives',
+		'Related_Material',
+		'Condition_Note',
+		'Time_Stamp',
+		'Timestamp_-_Last_Change',
+		'Cataloger'
+	);
 
 	/**
 	 * Show Reports view.
@@ -46,25 +92,32 @@ class ReportController extends Controller
 		->setTitle("AVCC - Report")
 		->setSubject("Report for all formats")
 		->setDescription("Report for all formats");
-		$phpExcelObject->setActiveSheetIndex(0)
-		->setCellValue('A1', 'Hello')
-		->setCellValue('B2', 'world!');
+		$activeSheet = $phpExcelObject->setActiveSheetIndex(0);
+		foreach ($this->columns as $column => $columnName)
+		{
+			$activeSheet->setCellValueExplicitByColumnAndRow($column, 1, $columnName);
+		}
+//		$activeSheet->setCellValue('A1', 'Hello');
+//		$activeSheet->setCellValue('B2', 'world!');
+//		$activeSheet->setCellValueExplicitByColumnAndRow(0, 1);
 		$phpExcelObject->getActiveSheet()->setTitle('All Formats');
 		// Set active sheet index to the first sheet, so Excel opens this as the first sheet
 		$phpExcelObject->setActiveSheetIndex(0);
+		$this->downloadReport($phpExcelObject);
+	}
 
+	private function downloadReport($phpExcelObject)
+	{
 		// create the writer
-		$writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel5');
+		$writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel2007');
 		// create the response
 		$response = $this->get('phpexcel')->createStreamedResponse($writer);
-		// adding headers
 		$response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
 		$response->headers->set('Content-Disposition', 'attachment;filename=stream-file.xls');
 		$response->headers->set('Pragma', 'public');
 		$response->headers->set('Cache-Control', 'maxage=1');
 
 		return $response;
-		return array();
 	}
 
 }
