51e51759d3cd1aefff9af99bd39803599cdaae30
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-20 12:15:35 +0500

    worked on reporting task

--- src/Application/Bundle/FrontBundle/Controller/ReportController.php
+++ src/Application/Bundle/FrontBundle/Controller/ReportController.php
@@ -104,7 +104,7 @@ class ReportController extends Controller
 		// Set active sheet index to the first sheet, so Excel opens this as the first sheet
 		$phpExcelObject->setActiveSheetIndex(0);
 		$this->downloadReport($phpExcelObject);
-		return array();
+		exit;
 	}
 
 	private function downloadReport($phpExcelObject)
