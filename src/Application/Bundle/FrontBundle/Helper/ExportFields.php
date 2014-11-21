97a71407ea727d4e41e1326682b7a3a484e82aec
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-21 15:10:23 +0500

    changes in reporting

---
+++ src/Application/Bundle/FrontBundle/Helper/ExportFields.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Application\Bundle\FrontBundle\Helper;
+
+class ExportFields
+{
+
+	private $columns = array(
+		'Project_Name',
+		'Collection_Name',
+		'Media_Type',
+		'Unique_ID',
+		'Location',
+		'Format',
+		'Title',
+		'Description',
+		'Commercial_or_Unique',
+		'Content_Duration',
+		'Media_Duration',
+		'Creation_Date',
+		'Content_Date',
+		'Base',
+		'Print_Type',
+		'Disk_Diameter',
+		'Reel_Diameter',
+		'Media_Diameter',
+		'Footage',
+		'Recording_Speed',
+		'Color',
+		'Tape_Thickness',
+		'Sides',
+		'Track_Type',
+		'Mono_or_Stereo',
+		'Noise_Reduction',
+		'Cassette_Size',
+		'Format_Version',
+		'Recording_Standard',
+		'Reel_or_Core',
+		'Sound',
+		'Frame_Rate',
+		'Acid_Detection_Strip',
+		'Shrinkage',
+		'Genre_Terms',
+		'Contributor',
+		'Generation',
+		'Part',
+		'Copyright_/_Restrictions',
+		'Duplicates_/_Derivatives',
+		'Related_Material',
+		'Condition_Note',
+		'Time_Stamp',
+		'Timestamp_-_Last_Change',
+		'Cataloger'
+	);
+
+	/**
+	 * Return array of columns for csv or xlsx tempate.
+	 * 
+	 * @return array
+	 */
+	public function getExportColumns()
+	{
+		return $this->columns;
+	}
+
+}
