9eb553b55651d62a69b59d320a6d065220c9d0b3
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-23 01:56:42 +0500

    code cleanup

--- src/Application/Bundle/FrontBundle/Controller/RecordsController.php
+++ src/Application/Bundle/FrontBundle/Controller/RecordsController.php
@@ -21,290 +21,277 @@ use Application\Bundle\FrontBundle\SphinxSearch\SphinxSearch;
 class RecordsController extends Controller
 {
 
-    private $columnOrder = array();
-    private $defaultFields;
-    private $columns = array();
-    private $userFields = array();
-    private $session;
-    private $offset;
-    private $limit;
+	private $columnOrder = array();
+	private $defaultFields;
+	private $columns = array();
+	private $userFields = array();
+	private $session;
+	private $offset;
+	private $limit;
 
-    public function __construct()
-    {
-        $this->columns = array(
-            'checkbox_Col' => 'checkboxCol',
-            'Project_Name' => 'projectName',
-            'Format' => 'format',
-            'Unique_ID' => 'uniqueId',
-            'Title' => 'title',
-            'Collection_Name' => 'collectionName',
-            'Location' => 'location'
-        );
-        $this->keywords = array('title',
-            'description', 'collection_name',
-            'creation_date', 'content_date', 'genre_terms', 'contributor',
-        );
-        $this->defaultFields = new DefaultFields();
-        $this->limit = 10;
-    }
+	public function __construct()
+	{
+		$this->columns = array(
+			'checkbox_Col' => 'checkboxCol',
+			'Project_Name' => 'projectName',
+			'Format' => 'format',
+			'Unique_ID' => 'uniqueId',
+			'Title' => 'title',
+			'Collection_Name' => 'collectionName',
+			'Location' => 'location'
+		);
+		$this->keywords = array('title',
+			'description', 'collection_name',
+			'creation_date', 'content_date', 'genre_terms', 'contributor',
+		);
+		$this->defaultFields = new DefaultFields();
+		$this->limit = 10;
+	}
 
-    /**
-     * Lists all AudioRecords entities.
-     *
-     * @Route("/", name="record_list")
-     * @Method("GET")
-     * @Template("ApplicationFrontBundle:Records:index.html.php")
-     * @return array
-     */
-    public function indexAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
+	/**
+	 * Lists all AudioRecords entities.
+	 *
+	 * @Route("/", name="record_list")
+	 * @Method("GET")
+	 * @Template("ApplicationFrontBundle:Records:index.html.php")
+	 * @return array
+	 */
+	public function indexAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
 
-        $shpinxInfo = $this->getSphinxInfo();
-        $sphinxSearch = new SphinxSearch($em, $shpinxInfo);
+		$shpinxInfo = $this->getSphinxInfo();
+		$sphinxSearch = new SphinxSearch($em, $shpinxInfo);
 
-        $isAjax = FALSE;
-        if ($request->isXmlHttpRequest()) {
-            $isAjax = TRUE;
-            $this->getFacetRequest($request);
-            $searchOn = $this->criteria();
-            $criteria = $searchOn['criteriaArr'];
-            $parentFacet = isset($searchOn['parent_facet'])? $searchOn['parent_facet'] : null;
-            $facet['mediaType'] = $sphinxSearch->facetSelect('media_type', $criteria, $parentFacet);
-            $facet['formats'] = $sphinxSearch->facetSelect('format', $criteria, $parentFacet);
-            $facet['commercialUnique'] = $sphinxSearch->facetSelect('commercial', $criteria, $parentFacet);
-            $facet['bases'] = $sphinxSearch->facetSelect('base', $criteria, $parentFacet);
-            $facet['recordingStandards'] = $sphinxSearch->facetSelect('recording_standard', $criteria, $parentFacet);
-            $facet['printTypes'] = $sphinxSearch->facetSelect('print_type', $criteria, $parentFacet);
-            $facet['projectNames'] = $sphinxSearch->facetSelect('project', $criteria, $parentFacet);
-            $facet['reelDiameters'] = $sphinxSearch->facetSelect('reel_diameter', $criteria, $parentFacet);
-            $facet['discDiameters'] = $sphinxSearch->facetSelect('disk_diameter', $criteria, $parentFacet);
-            $facet['acidDetection'] = $sphinxSearch->facetSelect('acid_detection', $criteria, $parentFacet);
-            $facet['collectionNames'] = $sphinxSearch->facetSelect('collection_name', $criteria, $parentFacet);
+		$isAjax = FALSE;
+		$parentFacet = null;
+		$criteria = null;
+		if ($request->isXmlHttpRequest())
+		{
+			$isAjax = TRUE;
+			$this->getFacetRequest($request);
+			$searchOn = $this->criteria();
+			$criteria = $searchOn['criteriaArr'];
+			$parentFacet = isset($searchOn['parent_facet']) ? $searchOn['parent_facet'] : null;
+		}
+		$facet['mediaType'] = $sphinxSearch->facetSelect('media_type', $criteria, $parentFacet);
+		$facet['formats'] = $sphinxSearch->facetSelect('format', $criteria, $parentFacet);
+		$facet['commercialUnique'] = $sphinxSearch->facetSelect('commercial', $criteria, $parentFacet);
+		$facet['bases'] = $sphinxSearch->facetSelect('base', $criteria, $parentFacet);
+		$facet['recordingStandards'] = $sphinxSearch->facetSelect('recording_standard', $criteria, $parentFacet);
+		$facet['printTypes'] = $sphinxSearch->facetSelect('print_type', $criteria, $parentFacet);
+		$facet['projectNames'] = $sphinxSearch->facetSelect('project', $criteria, $parentFacet);
+		$facet['reelDiameters'] = $sphinxSearch->facetSelect('reel_diameter', $criteria, $parentFacet);
+		$facet['discDiameters'] = $sphinxSearch->facetSelect('disk_diameter', $criteria, $parentFacet);
+		$facet['acidDetection'] = $sphinxSearch->facetSelect('acid_detection', $criteria, $parentFacet);
+		$facet['collectionNames'] = $sphinxSearch->facetSelect('collection_name', $criteria, $parentFacet);
+		$view = array(
+			'facets' => $facet,
+			'columns' => $this->columns,
+			'isAjax' => $isAjax
+		);
+		if ($request->isXmlHttpRequest())
+		{
+			$html = $this->render('ApplicationFrontBundle:Records:index.html.php', $view);
+			echo json_encode(array('html' => $html->getContent()));
+			exit;
+		}
+		else
+		{
+			return $view;
+		}
+	}
 
-            $html = $this->render('ApplicationFrontBundle:Records:index.html.php', array(
-                'facets' => $facet,
-                'columns' => $this->columns,
-                'isAjax' => $isAjax
-            ));
-            echo json_encode(array('html' => $html->getContent()));
-            exit;
-        }
-        $facet['mediaType'] = $sphinxSearch->facetSelect('media_type');
-        $facet['formats'] = $sphinxSearch->facetSelect('format');
-        $facet['commercialUnique'] = $sphinxSearch->facetSelect('commercial');
-        $facet['bases'] = $sphinxSearch->facetSelect('base');
-        $facet['recordingStandards'] = $sphinxSearch->facetSelect('recording_standard');
-        $facet['printTypes'] = $sphinxSearch->facetSelect('print_type');
-        $facet['projectNames'] = $sphinxSearch->facetSelect('project');
-        $facet['reelDiameters'] = $sphinxSearch->facetSelect('reel_diameter');
-        $facet['discDiameters'] = $sphinxSearch->facetSelect('disk_diameter');
-        $facet['acidDetection'] = $sphinxSearch->facetSelect('acid_detection');
-        $facet['collectionNames'] = $sphinxSearch->facetSelect('collection_name');
+	/**
+	 * Make records to display for dataTables.
+	 *
+	 * @param Request $request
+	 *
+	 * @Route("/dataTable", name="record_dataTable")
+	 * @Method("GET")
+	 * @Template("ApplicationFrontBundle:AudioRecords:dataTable.html.php")
+	 * @return json
+	 */
+	public function dataTableAction(Request $request)
+	{
+		$columns = array(0 => '',
+			1 => 'title',
+			2 => 'format',
+			3 => 'unique_id',
+			4 => 'title',
+			5 => 'collection_name',
+			6 => 'location'
+		);
+		$em = $this->getDoctrine()->getManager();
+		$sEcho = $request->query->get('sEcho');
+		$sortOrder = $request->query->get('sSortDir_0') ? $request->query->get('sSortDir_0') : 'asc';
+		$sortIndex = $request->query->get('iSortCol_0') ? $columns[$request->query->get('iSortCol_0')] : 'title';
 
-        return array(
-            'facets' => $facet,
-            'columns' => $this->columns,
-            'isAjax' => $isAjax
-        );
-    }
+		$offset = $request->query->get('iDisplayStart') ? $request->query->get('iDisplayStart') : 0;
+		$limit = $request->query->get('iDisplayLength') ? $request->query->get('iDisplayLength') : 10;
 
-    /**
-     * Make records to display for dataTables.
-     *
-     * @param Request $request
-     *
-     * @Route("/dataTable", name="record_dataTable")
-     * @Method("GET")
-     * @Template("ApplicationFrontBundle:AudioRecords:dataTable.html.php")
-     * @return json
-     */
-    public function dataTableAction(Request $request)
-    {
-        $columns = array(0 => '',
-            1 => 'title',
-            2 => 'format',
-            3 => 'unique_id',
-            4 => 'title',
-            5 => 'collection_name',
-            6 => 'location'
-        );
-        $em = $this->getDoctrine()->getManager();
-        $sEcho = $request->query->get('sEcho');
-        $sortOrder = $request->query->get('sSortDir_0') ? $request->query->get('sSortDir_0') : 'asc';
-        $sortIndex = $request->query->get('iSortCol_0') ? $columns[$request->query->get('iSortCol_0')] : 'title';
+		$shpinxInfo = $this->getSphinxInfo();
+		$sphinxSearch = new SphinxSearch($em, $shpinxInfo);
+		$searchOn = $this->criteria();
+		$criteria = $searchOn['criteriaArr'];
+		$result = $sphinxSearch->select($this->getUser(), $offset, $limit, $sortIndex, $sortOrder, $criteria);
 
-        $offset = $request->query->get('iDisplayStart') ? $request->query->get('iDisplayStart') : 0;
-        $limit = $request->query->get('iDisplayLength') ? $request->query->get('iDisplayLength') : 10;
+		$records = $result[0];
+		$currentPageTotal = count($records);
+		$totalRecords = $result[1][0]['Value'];
 
-        $shpinxInfo = $this->getSphinxInfo();
-        $sphinxSearch = new SphinxSearch($em, $shpinxInfo);
-        $searchOn = $this->criteria();
-        $criteria = $searchOn['criteriaArr'];        
-        $result = $sphinxSearch->select($this->getUser(), $offset, $limit, $sortIndex, $sortOrder, $criteria);
-//        print_r($result);
-//        exit;
-        $records = $result[0];
-        $currentPageTotal = count($records);
-        $totalRecords = $result[1][0]['Value'];
-//        print_r($result);exit;
 
-        $tableView = $this->defaultFields->recordDatatableView($records);
+		$tableView = $this->defaultFields->recordDatatableView($records);
 
-        $dataTable = array(
-            'sEcho' => intval($sEcho),
-            'iTotalRecords' => intval($currentPageTotal),
-            'iTotalDisplayRecords' => intval($totalRecords),
-            'aaData' => $tableView
-        );
-        echo json_encode($dataTable);
-        exit;
-    }
+		$dataTable = array(
+			'sEcho' => intval($sEcho),
+			'iTotalRecords' => intval($currentPageTotal),
+			'iTotalDisplayRecords' => intval($totalRecords),
+			'aaData' => $tableView
+		);
+		echo json_encode($dataTable);
+		exit;
+	}
 
-    /**
-     * Get sphinx parameters
-     *
-     * @return array
-     */
-    protected function getSphinxInfo()
-    {
-        return $this->container->getParameter('sphinx_param');
-    }
+	/**
+	 * Get sphinx parameters
+	 *
+	 * @return array
+	 */
+	protected function getSphinxInfo()
+	{
+		return $this->container->getParameter('sphinx_param');
+	}
 
-    /**
-     *
-     * @param Request $request
-     *
-     * @Route("/facets", name="record_facets")
-     * @Method("POST")
-     */
-    public function facetsAction(Request $request)
-    {
-        $data = $request->request->all();
-        $session = $this->getRequest()->getSession();
-        if ($data) {
-            $session->remove('facetData');
-            $session->set('facetData', $data);
-        } else {
-            $session->remove('facetData');
-        }
-        echo json_encode(array('success' => true));
-        exit;
-    }
+	/**
+	 *
+	 * @param Request $request
+	 *
+	 * @Route("/facets", name="record_facets")
+	 * @Method("POST")
+	 */
+	public function facetsAction(Request $request)
+	{
+		$data = $request->request->all();
+		$session = $this->getRequest()->getSession();
+		if ($data)
+		{
+			$session->remove('facetData');
+			$session->set('facetData', $data);
+		}
+		else
+		{
+			$session->remove('facetData');
+		}
+		echo json_encode(array('success' => true));
+		exit;
+	}
 
-    /**
-     * Set criteria for facets
-     *
-     * @return array
-     */
-    protected function criteria()
-    {
-        $criteria = null;
-        $criteriaArr = null;
-        $facetData = $this->getFacetFromSession();
-        
-        if (isset($facetData['mediaType'])) {
-            $criteriaArr['s_media_type'] = $facetData['mediaType'];
-        }
-        if (isset($facetData['commercial'])) {
-            $criteriaArr['s_commercial'] = $facetData['commercial'];
-        }
-        if (isset($facetData['format'])) {
-            $criteriaArr['s_format'] = $facetData['format'];
-        }
-        if (isset($facetData['base'])) {
-            $criteriaArr['s_base'] = $facetData['base'];
-        }
-        if (isset($facetData['collectionName'])) {
-            $criteriaArr['s_collection_name'] = $facetData['collectionName'];
-        }
-        if (isset($facetData['recordingStandard'])) {
-            $criteriaArr['s_recording_standard'] = $facetData['recordingStandard'];
-        }
-        if (isset($facetData['printType'])) {
-            $criteriaArr['s_print_type'] = $facetData['printType'];
-        }
-        if (isset($facetData['reelDiameter'])) {
-            $criteriaArr['s_reel_diameter'] = $facetData['reelDiameter'];
-        }
-        if (isset($facetData['discDiameter'])) {
-            $criteriaArr['s_disk_diameter'] = $facetData['discDiameter'];
-        }
-        if (isset($facetData['acidDetection'])) {
-            $criteriaArr['s_acid_detection'] = $facetData['acidDetection'];
-        }
-        if (isset($facetData['project'])) {
-            $criteriaArr['s_project'] = $facetData['project'];
-        }
-        if (isset($facetData['is_review_check'])) {
-            $criteriaArr['is_review'] = $facetData['is_review_check'];
-        }
-        if (isset($facetData['creationDate'])) {
-            $criteriaArr['s_creation_date'] = $facetData['creationDate'];
-        }
-        if (isset($facetData['contentDate'])) {
-            $criteriaArr['s_content_date'] = $facetData['contentDate'];
-        }
-        if ($facetData['facet_keyword_search']) {
-            $keywords = json_decode($facetData['facet_keyword_search'], true);
-            foreach ($keywords as $keyword) {
-                if ($keyword['type'] == 'all') {
-                    foreach ($this->keywords as $key) {
-                        $criteriaArr['*'] = $keyword['value'];
-                    }
-                } else {
-                    $criteriaArr['s_' . $keyword['type']] = $keyword['value'];
-                }
-            }
-        }
-        if($facetData['parent_facet']){
-            $criteria['parent_facet'] = $facetData['parent_facet'];
-        }
-        
-        if($criteriaArr){
-            $criteria['criteriaArr'] = $criteriaArr;
-        }
-        return $criteria;
-    }
+	/**
+	 * Set criteria for facets
+	 *
+	 * @return array
+	 */
+	protected function criteria()
+	{
+		$criteria = null;
+		$criteriaArr = null;
+		$facetData = $this->getFacetFromSession();
+		$searchColumns = array(
+			'mediaType' => 's_media_type',
+			'commercial' => 's_commercial',
+			'format' => 's_format',
+			'base' => 's_base',
+			'collectionName' => 's_collection_name',
+			'recordingStandard' => 's_recording_standard',
+			'printType' => 's_print_type',
+			'reelDiameter' => 's_reel_diameter',
+			'discDiameter' => 's_disk_diameter',
+			'acidDetection' => 's_acid_detection',
+			'project' => 's_project',
+			'is_review_check' => 'is_review',
+			'creationDate' => 's_creation_date',
+			'contentDate' => 's_content_date',
+			'parent_facet' => 'parent_facet',
+		);
+		foreach ($searchColumns as $key => $value)
+		{
+			if (isset($facetData[$key]))
+				$criteriaArr[$value] = $facetData[$key];
+		}
 
-    protected function getFacetRequest(Request $request)
-    {
-        $data = $request->query->all();
-        $session = $this->getRequest()->getSession();
-        if ($data) {
-            $session->remove('facetData');
-            $session->set('facetData', $data);
-        } else {
-            $session->remove('facetData');
-        }
-    }
 
-    protected function removeEmpty($array)
-    {
-        $result = array();
-        foreach ($array as $facet) {
-            foreach ($facet as $key => $value) {
-                if ($key != "total") {
-                    if ($value == '' && $value == null && empty($value)) {
-                        $result[][$key] = $value;
-                    }
-                }
-            }
-        }
+		if ($facetData['facet_keyword_search'])
+		{
+			$keywords = json_decode($facetData['facet_keyword_search'], true);
+			foreach ($keywords as $keyword)
+			{
+				if ($keyword['type'] == 'all')
+				{
+					foreach ($this->keywords as $key)
+					{
+						$criteriaArr['*'] = $keyword['value'];
+					}
+				}
+				else
+				{
+					$criteriaArr['s_' . $keyword['type']] = $keyword['value'];
+				}
+			}
+		}
+		if ($criteriaArr)
+		{
+			$criteria['criteriaArr'] = $criteriaArr;
+		}
+		return $criteria;
+	}
 
-        return $result;
-    }
-    
-    /**
-     * Get facets from session
-     * 
-     * @return array
-     */
-    protected function getFacetFromSession(){
-        $session = $this->getRequest()->getSession();
-        $facetData = $session->get('facetData');
-        
-        return $facetData;
-    }
+	protected function getFacetRequest(Request $request)
+	{
+		$data = $request->query->all();
+		$session = $this->getRequest()->getSession();
+		if ($data)
+		{
+			$session->remove('facetData');
+			$session->set('facetData', $data);
+		}
+		else
+		{
+			$session->remove('facetData');
+		}
+	}
+
+	protected function cleanEmptyData($array)
+	{
+		$result = array();
+		foreach ($array as $facet)
+		{
+			foreach ($facet as $key => $value)
+			{
+				if ($key != "total")
+				{
+					if ($value == '' && $value == null && empty($value))
+					{
+						$result[][$key] = $value;
+					}
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get facets from session
+	 * 
+	 * @return array
+	 */
+	protected function getFacetFromSession()
+	{
+		$session = $this->getRequest()->getSession();
+		$facetData = $session->get('facetData');
+
+		return $facetData;
+	}
 
 }
--- src/Application/Bundle/FrontBundle/SphinxSearch/SphinxSearch.php
+++ src/Application/Bundle/FrontBundle/SphinxSearch/SphinxSearch.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * File SphinxSearch
+ * 
+ * 
+ */
+
 namespace Application\Bundle\FrontBundle\SphinxSearch;
 
 use Foolz\SphinxQL\SphinxQL;
@@ -8,16 +14,55 @@ use Foolz\SphinxQL\Connection;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
+/**
+ * SphinxSearch is used to manage all operations for sphinxsearch service.
+ * It uses SphinxQL.
+ * 
+ */
 class SphinxSearch extends ContainerAware
 {
 
+	/**
+	 * Connection for sphinxQL.
+	 * @var Connection 
+	 */
 	private $conn;
+
+	/**
+	 * Name of the index.
+	 * @var string 
+	 */
 	private $indexName = 'records';
+
+	/**
+	 * Record ID.
+	 * @var integer 
+	 */
 	private $recordId;
+
+	/**
+	 * Entity manager.
+	 * 
+	 * @var EntityManager 
+	 */
 	private $entityManager = null;
+
+	/**
+	 * Record type
+	 * 
+	 * @var type 
+	 */
 	private $recordTypeId;
 
-	public function __construct(EntityManager $entityManager, $sphinxInfo, $recordId = null, $recordTypeId = null)
+	/**
+	 * Constructor of SphinxSearch
+	 * 
+	 * @param EntityManager $entityManager
+	 * @param array $sphinxInfo
+	 * @param integer $recordId
+	 * @param integer $recordTypeId
+	 */
+	public function __construct(EntityManager $entityManager, array $sphinxInfo, $recordId = null, $recordTypeId = null)
 	{
 		$this->entityManager = $entityManager;
 		$this->recordId = $recordId;
@@ -29,6 +74,11 @@ class SphinxSearch extends ContainerAware
 		$this->indexName = $sphinxInfo['indexName'];
 	}
 
+	/**
+	 * Insert new record to sphinx index.
+	 * 
+	 * @return array on count of records.
+	 */
 	public function insert()
 	{
 		$sphinxFields = new SphinxFields();
@@ -39,6 +89,13 @@ class SphinxSearch extends ContainerAware
 		return $sq->execute();
 	}
 
+	/**
+	 * Update the existing record in sphinx index.
+	 * 
+	 * One work for attr_unit.
+	 * 
+	 * @return array on count of records.
+	 */
 	public function update()
 	{
 		$sphinxFields = new SphinxFields();
@@ -49,6 +106,11 @@ class SphinxSearch extends ContainerAware
 		return $sq->execute();
 	}
 
+	/**
+	 * Replace the values for existing record.
+	 * 
+	 * @return array on count of records.
+	 */
 	public function replace()
 	{
 		$sphinxFields = new SphinxFields();
@@ -59,6 +121,18 @@ class SphinxSearch extends ContainerAware
 		return $sq->execute();
 	}
 
+	/**
+	 * Select record for listing from sphinx.
+	 * 
+	 * @param User    $user
+	 * @param integer $offset
+	 * @param integer $limit
+	 * @param string  $sortColumn
+	 * @param string  $sortOrder
+	 * @param string  $criteria
+	 * 
+	 * @return array
+	 */
 	public function select($user, $offset = 0, $limit = 100, $sortColumn = 'title', $sortOrder = 'asc', $criteria = null)
 	{
 		$sq = SphinxQL::create($this->conn);
@@ -79,6 +153,16 @@ class SphinxSearch extends ContainerAware
 		return $result;
 	}
 
+	/**
+	 * Select total found record for listing from sphinx.
+	 * 
+	 * @param integer $offset
+	 * @param integer $limit
+	 * @param string  $sortColumn
+	 * @param string  $sortOrder
+	 * 
+	 * @return array
+	 */
 	public function selectCount($offset = 0, $limit = 100, $sortColumn = 'title', $sortOrder = 'asc')
 	{
 		$sq = SphinxQL::create($this->conn)
@@ -91,6 +175,15 @@ class SphinxSearch extends ContainerAware
 		return $sq->executeBatch();
 	}
 
+	/**
+	 * Prepare facet for sphinx attribute.
+	 * 
+	 * @param string $facetColumn
+	 * @param array  $criteria
+	 * @param string $parentFacet
+	 * 
+	 * @return array
+	 */
 	public function facetSelect($facetColumn, $criteria = null, $parentFacet = false)
 	{
 		$sq = SphinxQL::create($this->conn)
@@ -98,7 +191,7 @@ class SphinxSearch extends ContainerAware
 		->from($this->indexName);
 		if ($criteria && $facetColumn != $parentFacet)
 		{
-			$this->whereClause($criteria, $sq, $parentFacet);
+			$this->whereClause($criteria, $sq);
 		}
 		$sq->where($facetColumn, '!=', '');
 		$sq->groupBy($facetColumn)
@@ -107,7 +200,15 @@ class SphinxSearch extends ContainerAware
 		return $sq->execute();
 	}
 
-	public function whereClause($criteria, $sq, $parentFacet = false)
+	/**
+	 * Make where clause for searching.
+	 * 
+	 * @param array    $criteria
+	 * @param SphinxQL $sq
+	 * 
+	 * @return void
+	 */
+	public function whereClause($criteria, $sq)
 	{
 		foreach ($criteria as $key => $value)
 		{
