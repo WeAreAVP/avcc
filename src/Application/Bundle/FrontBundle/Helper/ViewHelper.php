3ada12be808c9db25305143810d0d3722e0f6bce
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-23 16:05:37 +0500

    code cleanup

--- src/Application/Bundle/FrontBundle/Helper/ViewHelper.php
+++ src/Application/Bundle/FrontBundle/Helper/ViewHelper.php
@@ -7,59 +7,46 @@ use Symfony\Component\Templating\Helper\Helper;
 class ViewHelper extends Helper
 {
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getName()
-	{
-		return "myViewHelper";
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return "myViewHelper";
+    }
 
-	public function sortByOneKey(array $array, $key, $asc = TRUE)
-	{
-		$result = array();
+    public function sortByOneKey(array $array, $key, $asc = TRUE)
+    {
+        $result = array();
 
-		$values = array();
-		foreach ($array as $id => $value)
-		{
-			if (is_object($value))
-			{
-				$values[$id] = isset($value->$key) ? $value->$key : '';
-			}
-			else
-			{
-				$values[$id] = isset($value[$key]) ? $value[$key] : '';
-			}
-		}
+        $values = array();
+        foreach ($array as $id => $value) {
+            if (is_object($value)) {
+                $values[$id] = isset($value->$key) ? $value->$key : '';
+            } else {
+                $values[$id] = isset($value[$key]) ? $value[$key] : '';
+            }
+        }
 
-		if ($asc)
-		{
-			asort($values);
-		}
-		else
-		{
-			arsort($values);
-		}
+        if ($asc) {
+            asort($values);
+        } else {
+            arsort($values);
+        }
 
+        foreach ($values as $index => $value) {
+            $check_value = '';
+            if (is_object($array[$index])) {
+                $check_value = $array[$index]->$key;
+            } else
+                $check_value = $array[$index][$key];
+            if (trim($check_value) != '') {
 
-		foreach ($values as $index => $value)
-		{
-			$check_value = '';
-			if (is_object($array[$index]))
-			{
-				$check_value = $array[$index]->$key;
-			}
-			else
-				$check_value = $array[$index][$key];
-			if (trim($check_value) != '')
-			{
+                $result[] = $array[$index];
+            }
+        }
 
-				$result[] = $array[$index];
-			}
-		}
-
-
-		return $result;
-	}
+        return $result;
+    }
 
 }
