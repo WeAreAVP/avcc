adb420ce1c4e405b6f720cf968ce45da548499ad
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-23 14:51:01 +0500

    facet emtpy issue

--- src/Application/Bundle/FrontBundle/Controller/RecordsController.php
+++ src/Application/Bundle/FrontBundle/Controller/RecordsController.php
@@ -81,6 +81,8 @@ class RecordsController extends Controller
         $facet['discDiameters'] = $sphinxSearch->facetSelect('disk_diameter', $criteria, $parentFacet);
         $facet['acidDetection'] = $sphinxSearch->facetSelect('acid_detection', $criteria, $parentFacet);
         $facet['collectionNames'] = $sphinxSearch->facetSelect('collection_name', $criteria, $parentFacet);
+		$facet=  $this->removeEmpty($facet);
+		echo '<pre>';print_r($facet);exit;
         $view = array(
             'facets' => $facet,
             'columns' => $this->columns,
