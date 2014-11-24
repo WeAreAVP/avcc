8e7c4124bbfdd23720663b3a5e9fc33340c0e693
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-24 14:16:46 +0500

    added command line functionality

---
+++ src/Application/Bundle/FrontBundle/Command/ExportReportCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Application\Bundle\FrontBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExportReportCommand extends Command
+{
+
+	protected function configure()
+	{
+		$this
+		->setName('avcc:export-report')
+		->setDescription('Export the Records that are in queue and email to user.')
+		->addArgument(
+		'name', InputArgument::OPTIONAL, 'Who do you want to greet?'
+		)
+		->addOption(
+		'yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters'
+		)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$name = $input->getArgument('name');
+		if ($name)
+		{
+			$text = 'Hello ' . $name;
+		}
+		else
+		{
+			$text = 'Hello';
+		}
+
+		if ($input->getOption('yell'))
+		{
+			$text = strtoupper($text);
+		}
+
+		$output->writeln($text);
+	}
+
+}
