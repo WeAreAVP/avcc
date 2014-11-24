e2a646209f83f496a177df2106f27377b0e58e88
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-24 17:58:37 +0500

    command changes

--- src/Application/Bundle/FrontBundle/Command/ExportReportCommand.php
+++ src/Application/Bundle/FrontBundle/Command/ExportReportCommand.php
@@ -8,7 +8,12 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ExportReportCommand extends Command
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+
+
+
+class ExportReportCommand extends ContainerAwareCommand
 {
 
 	protected function configure()
