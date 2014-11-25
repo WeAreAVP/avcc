4309877e0d35a422530eccf399861b965666f4ac
Nouman Tayyab <noumantayyab@gmail.com>
2014-11-25 14:49:52 +0500

    sphinx changes

--- src/Application/Bundle/FrontBundle/Controller/ReportController.php
+++ src/Application/Bundle/FrontBundle/Controller/ReportController.php
@@ -17,61 +17,65 @@ use Application\Bundle\FrontBundle\SphinxSearch\SphinxSearch;
 class ReportController extends Controller
 {
 
-    /**
-     * Show Reports view.
-     *
-     * @Route("/", name="report")
-     * @Method("GET")
-     * @Template()
-     * @return array
-     */
-    public function indexAction()
-    {
-        return array();
-    }
-
-    /**
-     * Generate report as xlsx or csv
-     *
-     * @Route("/allformats/{type}", name="all_formats")
-     * @Method("GET")
-     * @Template()
-     * @return array
-     */
-    public function allFormatsAction($type)
-    {
-        if ( ! in_array($type, array('csv', 'xlsx'))) {
-            throw $this->createNotFoundException('Invalid report type');
-        }
-
-        $entityManager = $this->getDoctrine()->getManager();
-        if (true === $this->get('security.context')->isGranted('ROLE_SUPER_ADMIN'))
-            $records = $entityManager->getRepository('ApplicationFrontBundle:Records')->findAll();
-        else
-            $records = $entityManager->getRepository('ApplicationFrontBundle:Records')->findOrganizationRecords($this->getUser()->getOrganizations()->getId());
-
-        $exportComponent = new ExportReport($this->container);
-        $phpExcelObject = $exportComponent->generateReport($records);
-        $response = $exportComponent->outputReport($type, $phpExcelObject);
-
-        // create the response
-        return $response;
-        return array();
-    }
 	/**
-     * Generate quantitative report
-     *
-     * @Route("/quantitative", name="quantitative")
-     * @Method("GET")
-     * @Template()
-     * @return array
-     */
-	public function quantitativeAction(){
+	 * Show Reports view.
+	 *
+	 * @Route("/", name="report")
+	 * @Method("GET")
+	 * @Template()
+	 * @return array
+	 */
+	public function indexAction()
+	{
+		return array();
+	}
+
+	/**
+	 * Generate report as xlsx or csv
+	 *
+	 * @Route("/allformats/{type}", name="all_formats")
+	 * @Method("GET")
+	 * @Template()
+	 * @return array
+	 */
+	public function allFormatsAction($type)
+	{
+		if ( ! in_array($type, array('csv', 'xlsx')))
+		{
+			throw $this->createNotFoundException('Invalid report type');
+		}
+
+		$entityManager = $this->getDoctrine()->getManager();
+		if (true === $this->get('security.context')->isGranted('ROLE_SUPER_ADMIN'))
+			$records = $entityManager->getRepository('ApplicationFrontBundle:Records')->findAll();
+		else
+			$records = $entityManager->getRepository('ApplicationFrontBundle:Records')->findOrganizationRecords($this->getUser()->getOrganizations()->getId());
+
+		$exportComponent = new ExportReport($this->container);
+		$phpExcelObject = $exportComponent->generateReport($records);
+		$response = $exportComponent->outputReport($type, $phpExcelObject);
+
+		// create the response
+		return $response;
+		return array();
+	}
+
+	/**
+	 * Generate quantitative report
+	 *
+	 * @Route("/quantitative", name="quantitative")
+	 * @Method("GET")
+	 * @Template()
+	 * @return array
+	 */
+	public function quantitativeAction()
+	{
 		$em = $this->getDoctrine()->getManager();
 		$shpinxInfo = $this->container->getParameter('sphinx_param');
 		$sphinxSearch = new SphinxSearch($em, $shpinxInfo);
-		$sphinxSearch->removeEmpty($sphinxSearch->facetSelect('format', $criteria, $parentFacet), 'format');
-	return array();	
+		$result = $sphinxSearch->removeEmpty($sphinxSearch->facetSelect('format', $criteria, $parentFacet), 'format');
+		echo '<pre>';print_r($result);exit;
+		return array();
 	}
 
 }
