6100242c9195785f5c6c2f6b5d20d8b4ea959ccc
Nouman <nouman@avpreserve.com>
2014-11-26 15:00:16 +0500

    export records

--- src/Application/Bundle/FrontBundle/Resources/views/Records/_modal.html.php
+++ src/Application/Bundle/FrontBundle/Resources/views/Records/_modal.html.php
@@ -3,6 +3,6 @@
     <p><span style="font-size:13px;">Are you sure you want to export the record(s)?</span></p>
     <p class="pull-right">
         <button type="button" name="close" id="close" class="button closeModal simplemodal-close">No</button> &nbsp; 
-        <button type="button" name="yes" id="yes" class="button primary" id="exportRequest">Yes</button>
+        <button type="button" name="yes" class="button primary" id="exportRequest">Yes</button>
     </p>
 </div>
