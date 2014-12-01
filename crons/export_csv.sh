47e8fa2bcf52dc65f68a7975ed037dad1f1c7faf
Nouman <nouman@avpreserve.com>
2014-12-01 12:37:07 +0500

    cron setup

---
+++ crons/export_csv.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#####################################
+#                                   #
+#  Shell Script for sending email   #
+#  to users with records csv export #
+#  This script will prevent         #
+#  from running method more         #
+#  than one time                    #
+#  Developer : Nouman Tayyab        #
+#  nouman@geekschicago.com          #
+#                                   #
+#####################################
+
+#source /root/.bash_profile
+
+#HOST=`/bin/hostname`
+PID_FILE="PIDs/export_csv.pid"
+OUTPUT_FILE="cronlogs/export_csv.log"
+BASEDIR="avcc/avccqa/"
+DATE=`date`
+
+PHP_PATH=`which php`
+PID_FULLPATH=$BASEDIR$PID_FILE
+OUTPUT_FILE=$BASEDIR$OUTPUT_FILE
+
+touch $PID_FULLPATH
+touch $OUTPUT_FILE
+
+PID=`cat $PID_FULLPATH`
+PIDD=/proc/$PID 
+if [ -n "$PID" ]  && [ -d $PIDD ]
+then
+echo "Already running ...."
+else
+CMD="$PHP_PATH ${BASEDIR}app/console jms-job-queue:run > $OUTPUT_FILE 2>&1 & echo $! > $PID_FULLPATH"
+MSG="ENV [$ENVIRONMENT]"
+echo $MSG >> $OUTPUT_FILE
+MSG="Starting cron at $DATE"
+echo $MSG >> $OUTPUT_FILE
+echo $CMD >> $OUTPUT_FILE
+
+$PHP_PATH ${BASEDIR}app/console jms-job-queue:run >> $OUTPUT_FILE 2>&1 & echo $! > $PID_FULLPATH
+PID=`cat $PID_FULLPATH`
+echo "Started Cron [$PID]"
+echo "To view logs Use tail -f $OUTPUT_FILE"
+fi
