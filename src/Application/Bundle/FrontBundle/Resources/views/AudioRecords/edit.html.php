b49cb41478c8f22b711ab00b72fa034e0bd8077b
Nouman <nouman@avpreserve.com>
2014-12-05 11:42:15 +0500

    task 120, 125

--- src/Application/Bundle/FrontBundle/Resources/views/AudioRecords/edit.html.php
+++ src/Application/Bundle/FrontBundle/Resources/views/AudioRecords/edit.html.php
@@ -4,7 +4,7 @@
 <div class="grid fluid">
     <h1>
         <a href="<?php echo $view['router']->generate('record_list') ?>"><i class="icon-arrow-left-3 fg-darker smaller"></i> </a>
-        Edit Record<?php // echo ucwords($type) ?>
+        Edit Record<?php // echo ucwords($type)  ?>
     </h1>
     <?php // echo $view['form']->widget($edit_form); exit;?>
     <?php echo $view['form']->start($edit_form) ?>
@@ -16,25 +16,28 @@
             <?php
             $field = explode('.', $audioField['field']);
             ?>
-            <div style="<?php echo ($audioField['hidden']) ? 'display:none;' : ''; ?>" class="col-lg-6" id="<?php echo (count($field) == 2) ? $field[1].'_lbl' : $field[0].'_lbl' ?>">
-                <?php echo $view['form']->label((count($field) == 2) ? $edit_form[$field[0]][$field[1]] : $edit_form[$field[0]], ' ');echo $audioField['title'] ?>
-                <div class="input-control <?php echo (count($field) == 2 && $field[1] == 'isReview') ? '' : 'text';?>" data-role="input-control">
-                    <?php echo $view['form']->widget((count($field) == 2) ? $edit_form[$field[0]][$field[1]] : $edit_form[$field[0]], array('id' => (count($field) == 2) ? $field[1] : $field[0], 'attr' => array('class' => 'size4'))) ?>
+            <div style="<?php echo ($audioField['hidden']) ? 'display:none;' : ''; ?>" class="col-lg-6" id="<?php echo (count($field) == 2) ? $field[1] . '_lbl' : $field[0] . '_lbl' ?>">
+                <?php echo $view['form']->label((count($field) == 2) ? $edit_form[$field[0]][$field[1]] : $edit_form[$field[0]], ' ');
+                echo $audioField['title'] ?>
+                <div class="input-control <?php echo (count($field) == 2 && $field[1] == 'isReview') ? '' : 'text'; ?>" data-role="input-control">
+    <?php echo $view['form']->widget((count($field) == 2) ? $edit_form[$field[0]][$field[1]] : $edit_form[$field[0]], array('id' => (count($field) == 2) ? $field[1] : $field[0], 'attr' => array('class' => 'size4'))) ?>
                     <span class="has-error text-danger"><?php echo $view['form']->errors((count($field) == 2) ? $edit_form[$field[0]][$field[1]] : $edit_form[$field[0]]) ?></span>
                 </div>
             </div>
-        <?php endforeach; ?>
+    <?php endforeach; ?>
     </fieldset>
     <?php echo $view['form']->widget($edit_form['record']['userId']) ?>
     <?php echo $view['form']->widget($edit_form['record']['mediaTypeHidden']) ?>
-    <?php echo $view['form']->widget($edit_form['submit'], array('attr' => array('class' => 'button primary'))) ?>
-    <?php echo $view['form']->widget($edit_form['save_and_duplicate']) ?>
-    <?php echo $view['form']->end($edit_form) ?>
+    <a href="<?php echo $view['router']->generate('record_list') ?>" name="cancle" class="button">Cancel</a>
+    <?php echo $view['form']->widget($form['submit'], array('attr' => array('class' => 'button primary'))) ?>
+    <?php echo $view['form']->widget($form['save_and_new']) ?>
+    <?php echo $view['form']->widget($form['save_and_duplicate']) ?>
+<?php echo $view['form']->end($edit_form) ?>
 </div>
 <script src="<?php echo $view['assets']->getUrl('js/manage.records.js') ?>"></script>
 <script type="text/javascript">
-    var baseUrl = '<?php echo $view['router']->generate('record')?>';
-    var selectedFormat = '<?php echo $entity->getRecord()->getFormat()->getId();?>';
+    var baseUrl = '<?php echo $view['router']->generate('record') ?>';
+    var selectedFormat = '<?php echo $entity->getRecord()->getFormat()->getId(); ?>';
     $(document).ready(function () {
         initialize_records_form();
     });
