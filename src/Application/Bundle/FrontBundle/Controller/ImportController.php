7d0e60729d044fbded474f7f477d2d46b6963b19
Nouman <nouman@avpreserve.com>
2014-12-09 18:47:22 +0500

    import records

--- src/Application/Bundle/FrontBundle/Controller/ImportController.php
+++ src/Application/Bundle/FrontBundle/Controller/ImportController.php
@@ -29,12 +29,104 @@ class ImportController extends Controller
     /**
      * Import file 
      *
-     * @Route("/", name="import_records")
+     * @Route("/", name="import_index")
      * @Method("POST")
      * @Template("ApplicationFrontBundle:Records:default.html.php")
      * @return array
      */
     public function indexAction(Request $request)
+    {
+//        $em = $this->getDoctrine()->getManager();
+//        $data = $request->request->all();
+//        $type = $data['impfiletype'];
+//        if ($request->files->get('impfile')) {
+//            $originalFileName = $request->files->get('importfile')->getClientOriginalName();
+//            $uploadedFileSize = $request->files->get('importfile')->getClientSize();
+//            $newFileName = null;
+//            if ($uploadedFileSize > 0) {
+//                $folderPath = $this->container->getParameter('webUrl') . 'import/' . date('Y') . '/' . date('m') . '/';
+//                if (!is_dir($folderPath))
+//                    mkdir($folderPath, 0777, TRUE);
+//                $extension = $request->files->get('importfile')->getClientOriginalExtension();
+//                $newFileName = $this->getUser()->getId() . "_import" . time() . "." . $extension;
+//                if ($type == $extension) {
+//                    $request->files->get('importfile')->move($folderPath, $newFileName);
+//                    if (!$request->files->get('importfile')->isValid()) {
+//                        echo 'file uploaded';
+//                    }
+//                    $import = new ImportExport();
+//                    $import->setUser($this->getUser());
+//                    $import->setFormat($type);
+//                    $import->setType("import");
+//                    $import->setFileName($newFileName);
+//                    $import->setStatus(0);
+//                    $em->persist($import);
+//                    $em->flush();
+////
+////                    $job = new Job('avcc:import-records', array('id' => $import->getId()));
+////                    $date = new DateTime();
+////                    $date->add(new DateInterval('PT1M'));
+////                    $job->setExecuteAfter($date);
+////                    $em->persist($job);
+////                    $em->flush($job);
+//
+//                    $this->get('session')->getFlashBag()->add('export_merge', 'Import request successfully sent. You will receive an email shortly with download link.');
+//                } else {
+//                    $this->get('session')->getFlashBag()->add('export_merge_error', 'File formate is not correct. Please try again.');
+//                }
+//            } else {
+//                $this->get('session')->getFlashBag()->add('export_merge_error', 'File is empty. Please try again.');
+//            }
+//        } else {
+//            $this->get('session')->getFlashBag()->add('export_merge_error', 'Select file that require to import. Please try again.');
+//        }
+//
+//        return $this->redirect($this->generateUrl('record_list'));
+    }
+
+    /**
+     * Import file 
+     *
+     * @Route("/import", name="import_record")
+     * @Method("GET")
+     * @Template("ApplicationFrontBundle:Records:default.html.php")
+     * @return array
+     */
+    public function importAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $fileName = 'allFormat_1417781547.csv';
+        $import = new ImportReport($this->container);
+        $validation = $import->validateVocabulary($fileName);
+
+        if ($validation) {
+            echo '<pre>';
+            foreach ($validation as $key => $value){
+                echo '<p><b>'. str_replace('_', ' ', ucfirst($key)) . '</b><br />';
+                echo implode('<br />', $value);
+                echo '</p>';
+            }
+        }else{
+            $numberOfRecords = $import->getRecordsFromFile($fileName, $this->getUser());
+            echo '<pre>';
+            print_r($numberOfRecords);
+        }
+
+
+        exit;
+    }
+
+    /**
+     * Import file 
+     * 
+     * @param Request $request
+     * 
+     * @Route("/importRecords", name="import_records")
+     * @Method("POST")
+     * @Template("ApplicationFrontBundle:Records:default.html.php")
+     * @return array
+     */
+    public function importRecordsAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $data = $request->request->all();
@@ -83,37 +175,4 @@ class ImportController extends Controller
 
         return $this->redirect($this->generateUrl('record_list'));
     }
-
-    /**
-     * Import file 
-     *
-     * @Route("/import", name="import_record")
-     * @Method("GET")
-     * @Template("ApplicationFrontBundle:Records:default.html.php")
-     * @return array
-     */
-    public function importAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $fileName = 'allFormat_1417781547.csv';
-        $import = new ImportReport($this->container);
-        $validation = $import->validateVocabulary($fileName);
-
-        if ($validation) {
-            echo '<pre>';
-            foreach ($validation as $key => $value){
-                echo '<p><b>'. str_replace('_', ' ', ucfirst($key)) . '</b><br />';
-                echo implode('<br />', $value);
-                echo '</p>';
-            }
-        }else{
-            $numberOfRecords = $import->getRecordsFromFile($fileName, $this->getUser());
-            echo '<pre>';
-            print_r($numberOfRecords);
-        }
-
-
-        exit;
-    }
-
 }
