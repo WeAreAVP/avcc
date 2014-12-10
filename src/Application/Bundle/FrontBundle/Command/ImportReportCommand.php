05b04ba87d59fbc3dc0632276c717224e02b9717
Nouman <nouman@avpreserve.com>
2014-12-10 10:54:20 +0500

    import

--- src/Application/Bundle/FrontBundle/Command/ImportReportCommand.php
+++ src/Application/Bundle/FrontBundle/Command/ImportReportCommand.php
@@ -50,14 +50,14 @@ class ImportReportCommand extends ContainerAwareCommand
                         $templateParameters = array('user' => $entity->getUser(), 'numberOfRecords' => $numberOfRecords);
                     }
                 }
-                $rendered = $this->getContainer()->get('templating')->render('ApplicationFrontBundle:Records:import.email.html.php', $templateParameters);
-                $email = new EmailHelper($this->getContainer());
-                $subject = 'Import Report';
-                $email->sendEmail($rendered, $subject, $this->getContainer()->getParameter('from_email'), $user->getEmail());
+//                $rendered = $this->getContainer()->get('templating')->render('ApplicationFrontBundle:Records:import.email.html.php', $templateParameters);
+//                $email = new EmailHelper($this->getContainer());
+//                $subject = 'Import Report';
+//                $email->sendEmail($rendered, $subject, $this->getContainer()->getParameter('from_email'), $user->getEmail());
 //                $entity->setStatus(1);
 //                $em->persist($entity);
 //                $em->flush();
-                $text = $rendered;
+                $text = $numberOfRecords;
             } else {
                 $text = 'import id not found';
             }
