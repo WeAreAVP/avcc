f9ee6210f0a912738b6701dcb822992201cd82ec
Nouman <nouman@avpreserve.com>
2014-12-11 12:56:10 +0500

    bug fixing

--- src/Application/Bundle/FrontBundle/Resources/views/AudioRecords/getBase.html.php
+++ src/Application/Bundle/FrontBundle/Resources/views/AudioRecords/getBase.html.php
@@ -1,12 +1,16 @@
 <?php $view->extend('FOSUserBundle::default.layout.html.php') ?>
 <?php $view['slots']->start('body') ?>
+<?php if ($bases): ?>
 <option value=""></option>
 <?php
- foreach ($bases as $base) {
+foreach ($bases as $base) {
 ?>
-<option value="<?php echo $base->getId()?>"><?php echo $base->getName()?></option>
+<option value="<?php echo $base->getId() ?>"><?php echo $base->getName() ?></option>
 <?php
- }
+}
+?>
+<?php
+endif;
 ?>
 <?php
 $view['slots']->stop();
--- src/Application/Bundle/FrontBundle/Resources/views/AudioRecords/getRecordingSpeed.html.php
+++ src/Application/Bundle/FrontBundle/Resources/views/AudioRecords/getRecordingSpeed.html.php
@@ -1,12 +1,16 @@
 <?php $view->extend('FOSUserBundle::default.layout.html.php') ?>
 <?php $view['slots']->start('body') ?>
-<option value=""></option>
-<?php
- foreach ($speeds as $speed) {
-?>
-<option value="<?php echo $speed->getId()?>"><?php echo $speed->getName()?></option>
-<?php
- }
+<?php if ($speeds): ?>
+    <option value=""></option>
+    <?php
+    foreach ($speeds as $speed) {
+        ?>
+        <option value="<?php echo $speed->getId() ?>"><?php echo $speed->getName() ?></option>
+        <?php
+    }
+    ?>
+    <?php
+endif;
 ?>
 <?php
 $view['slots']->stop();
