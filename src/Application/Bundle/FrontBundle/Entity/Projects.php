1aa1b319d83f5c89cb2c2b382feed07fcc3e6f7e
Nouman <nouman@avpreserve.com>
2014-12-19 18:08:54 +0500

    organization required field in project

--- src/Application/Bundle/FrontBundle/Entity/Projects.php
+++ src/Application/Bundle/FrontBundle/Entity/Projects.php
@@ -50,7 +50,7 @@ class Projects
      *
      * @ORM\ManyToOne(targetEntity="Application\Bundle\FrontBundle\Entity\Organizations")
      * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="organization_id", referencedColumnName="id")
+     *   @ORM\JoinColumn(name="organization_id", referencedColumnName="id", nullable=false)
      * })
      */
     private $organization;
