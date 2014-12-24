f80bdb5ddbd00eed8b40bd3968c28f4ebe9fb6b5
Nouman <nouman@avpreserve.com>
2014-12-24 16:14:50 +0500

    file size report

---
+++ src/Application/Bundle/FrontBundle/Resources/views/Report/fileSize.html.php
@@ -0,0 +1,10 @@
+<?php $view->extend('FOSUserBundle::layout.html.php') ?>
+<?php $view['slots']->start('body') ?>
+<div class="grid">
+    <div class="row">
+
+    </div>
+</div>
+</div>
+<?php
+$view['slots']->stop();
