144a5cc6a2823bd16531980a76c5b0b65da9c0ef
Nouman Tayyab <noumantayyab@gmail.com>
2015-01-28 18:30:56 +0500

    task 144 - format json array

--- src/Application/Bundle/FrontBundle/Resources/public/js/dashboard.js
+++ src/Application/Bundle/FrontBundle/Resources/public/js/dashboard.js
@@ -147,7 +147,7 @@ function Dashboard() {
 //                                                data: query,
                                                 async: false,
 //                                                content: "application/json",
-                                                datatype: "json",
+                                                dataType: "json",
                                                 type: "GET",
                                                 success: function (response) {
                                                     data = response;
