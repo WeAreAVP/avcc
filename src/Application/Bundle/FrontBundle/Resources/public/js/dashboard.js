2690af41ea2983da632fddeada769002d2772df6
Nouman Tayyab <noumantayyab@gmail.com>
2015-01-28 18:32:52 +0500

    task 144 - format json array

--- src/Application/Bundle/FrontBundle/Resources/public/js/dashboard.js
+++ src/Application/Bundle/FrontBundle/Resources/public/js/dashboard.js
@@ -33,136 +33,68 @@ function Dashboard() {
             var selectedProject = $(this).val();
             if (selectedProject) {
                 url = baseUrl + 'getFormatCount/' + selectedProject;
-//                formats = [["1 Inch Open Reel Audio",4],["1 Inch Open Reel Video",2],["1\/2 Inch Open Reel Audio",4],["1\/2 Inch Open Reel Audio - Digital",4],["1\/2 Inch Open Reel Video",1],["1\/4 Inch Open Reel Audio",29],["1\/4 Inch Open Reel Video",2],["1610\/1630 (U-matic)",2],["16mm",5],["17mm",1],["2 Inch Open Reel Audio",3],["2 Inch Open Reel Video",2],["35mm",1],["45 RPM Disc",2],["70mm",3],["78 RPM Disc",3],["8-Track",3],["8mm",11],["9.5mm",1],["ADAT (VHS)",8]];
-////                chart = new Highcharts.Chart({
-//                $('#formatCount').highcharts({
-//                    chart: {
-////                        renderTo: 'formatCount',
-//                        type: 'column'
-//                    },
-//                    title: {
-//                        text: ''
-//                    },
-//                    subtitle: {
-//                        text: ''
-//                    },
-//                    credits: {
-//                        enabled: false
-//                    },
-//                    xAxis: {
-//                        type: 'category',
-//                        labels: {
-//                            rotation: -90,
-//                            style: {
-//                                fontSize: '13px',
-//                                fontFamily: 'Verdana, sans-serif'
-//                            }
-//                        }
-//                    },
-//                    yAxis: {
-//                        title: {
-//                            text: 'Total count per format'
-//                        }
-//                    },
-//                    legend: {
-//                        enabled: false
-//                    },
-//                    plotOptions: {
-//                        series: {
-//                            borderWidth: 0,
-//                            dataLabels: {
-//                                enabled: true,
-//                                format: '{point.y}'
-//                            }
-//                        }
-//                    },
-//                    tooltip: {
-//                        headerFormat: '<span><b>{series.name}: </b></span>',
-//                        pointFormat: '<span style="color:{point.color}">{point.name}</span><br/><b>Total:</b>{point.y}<br/>'
-//                    },
-//                    series: [{
-//                            name: 'Format',
-//                            colorByPoint: true,
-//                            data: formats
-//                        }]
-//                });
-
-//                $.ajax({
-//                    type: "GET",
-//                    url: url,
-//                    success: function (response) {
-//                        if (response != "") {
-//                            console.log(response);
-                            $('#formatCount').highcharts({
-                                chart: {
-                                    type: 'column'
-                                },
-                                title: {
-                                    text: ''
-                                },
-                                subtitle: {
-                                    text: ''
-                                },
-                                credits: {
-                                    enabled: false
-                                },
-                                xAxis: {
-                                    type: 'category',
-                                    labels: {
-//                                        rotation: -90,
-                                        style: {
-                                            fontSize: '13px',
-                                            fontFamily: 'Verdana, sans-serif'
-                                        }
+                $('#formatCount').highcharts({
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: ''
+                    },
+                    subtitle: {
+                        text: ''
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    xAxis: {
+                        type: 'category',
+                        labels: {
+                                        rotation: -90,
+                            style: {
+                                fontSize: '13px',
+                                fontFamily: 'Verdana, sans-serif'
+                            }
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Total count per format'
+                        }
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    plotOptions: {
+                        series: {
+                            borderWidth: 0,
+                            dataLabels: {
+                                enabled: true,
+                                format: '{point.y}'
+                            }
+                        }
+                    },
+                    tooltip: {
+                        headerFormat: '<span><b>{series.name}: </b></span>',
+                        pointFormat: '<span style="color:{point.color}">{point.name}</span><br/><b>Total:</b>{point.y}<br/>'
+                    },
+                    series: [{
+                            name: 'Format',
+                            colorByPoint: true,
+                            data: (function () {
+                                var data;
+                                $.ajax({
+                                    url: url,
+                                    async: false,
+                                    dataType: "json",
+                                    type: "GET",
+                                    success: function (response) {
+                                        data = response;
                                     }
-                                },
-                                yAxis: {
-                                    title: {
-                                        text: 'Total count per format'
-                                    }
-                                },
-                                legend: {
-                                    enabled: false
-                                },
-                                plotOptions: {
-                                    series: {
-                                        borderWidth: 0,
-                                        dataLabels: {
-                                            enabled: true,
-                                            format: '{point.y}'
-                                        }
-                                    }
-                                },
-                                tooltip: {
-                                    headerFormat: '<span><b>{series.name}: </b></span>',
-                                    pointFormat: '<span style="color:{point.color}">{point.name}</span><br/><b>Total:</b>{point.y}<br/>'
-                                },
-                                series: [{
-                                        name: 'Format',
-                                        colorByPoint: true,
-                                        data: (function () {
-                                            var data;
-                                            $.ajax({
-                                                url: url,
-//                                                data: query,
-                                                async: false,
-//                                                content: "application/json",
-                                                dataType: "json",
-                                                type: "GET",
-                                                success: function (response) {
-                                                    data = response;
-                                                    console.log(data);
-                                                    
-                                                }
-                                            });
-                                            return data;
-                                        })()
-                                    }]
-                            });
-//                        }
-//                    }
-//
-//                }); // Ajax Call 
+                                });
+                                return data;
+                            })()
+                        }]
+                });
             }
         }).change();
     }
