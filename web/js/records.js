13c46fe855f9d099fd86ac77140c300a676438d1
Nouman Tayyab <noumantayyab@gmail.com>
2015-02-23 18:37:22 +0500

    records checkbox debugging

--- web/js/records.js
+++ web/js/records.js
@@ -485,7 +485,10 @@ function Records() {
                 } else {
                     selectedrecords = id;
                 }
+                console.log("true");
+                console.log(selectedrecords);
             }else{
+                console.log("false");
                 checked = 0;
             }
         }
