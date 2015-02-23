d1d106edc89982959df370c152e2e53dff6dfe12
Nouman Tayyab <noumantayyab@gmail.com>
2015-02-23 18:32:47 +0500

    records checkbox debugging

--- web/js/records.js
+++ web/js/records.js
@@ -144,15 +144,15 @@ function Records() {
                         }
                     });
             $('#records tbody').on('click', 'tr', function () {
-                var id = this.id;
-                var rowid = id.split('-');
-                var index = $.inArray(rowid[1], selected);
-                if (index === -1) {
-                    selected.push(rowid[1]);
-                } else {
-                    selected.splice(index, 1);
-                }
-                console.log(rowid);
+//                var id = this.id;
+//                var rowid = id.split('-');
+//                var index = $.inArray(rowid[1], selected);
+//                if (index === -1) {
+//                    selected.push(rowid[1]);
+//                } else {
+//                    selected.splice(index, 1);
+//                }
+//                console.log(rowid);
 //                $(this).toggleClass('selected', function () {
 //                    var input = $("#" + id + " td:first").html();
 //                    if ($(this).hasClass('selected') === true) {
@@ -485,6 +485,8 @@ function Records() {
                 } else {
                     selectedrecords = id;
                 }
+            }else{
+                checked = 0;
             }
         }
         $("#selectedrecords").val(selectedrecords);
