2fd887888f2c2309e213b5f39d6f7309f95a8605
Nouman Tayyab <noumantayyab@gmail.com>
2015-02-26 14:06:13 +0500

    implementing sphinx command

--- src/Application/Bundle/FrontBundle/SphinxSearch/SphinxSearch.php
+++ src/Application/Bundle/FrontBundle/SphinxSearch/SphinxSearch.php
@@ -299,6 +299,7 @@ class SphinxSearch extends ContainerAware {
         $sq->select()
                 ->from($this->indexName);
         $sq->where('id', "=", $this->recordId);
+        $sq->where('media_type', "=", $this->recordTypeId);
         return $sq->execute();
     }
 
