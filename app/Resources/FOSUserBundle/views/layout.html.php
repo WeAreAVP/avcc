f60d8cf7c021e8f8e4d6f61068cdc399ef91d612
Nouman Tayyab <noumantayyab@gmail.com>
2015-03-16 16:11:02 +0500

    AVCC-206 in-progress

--- app/Resources/FOSUserBundle/views/layout.html.php
+++ app/Resources/FOSUserBundle/views/layout.html.php
@@ -147,7 +147,16 @@
         <script type="text/javascript" src="<?php echo $view['assets']->getUrl('js/chosen.jquery.js') ?>"></script>
         <script type="text/javascript" src="<?php echo $view['assets']->getUrl('js/jquery.mCustomScrollbar.concat.min.js') ?>"></script>
         <script type="text/javascript" src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-
+       <?php if(ga_tracking){ ?>
+            <script>
+                (function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function(){
+                (i[r].q = i[r].q || []).push(arguments)}, i[r].l = 1 * new Date(); a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+                })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+                        ga('create', {{ga_tracking}}, 'auto');
+                        ga('send', 'pageview');
+            </script>
+        <?php } ?>
         <script type="text/javascript">
             setTimeout(function () {
                 $('.text-success').hide();
