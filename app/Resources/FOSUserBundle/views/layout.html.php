02cb2a8987794db0745f64d7c974670754b09bd4
Nouman Tayyab <noumantayyab@gmail.com>
2015-03-16 16:27:29 +0500

    AVCC-206 #in-progress

--- app/Resources/FOSUserBundle/views/layout.html.php
+++ app/Resources/FOSUserBundle/views/layout.html.php
@@ -147,13 +147,13 @@
         <script type="text/javascript" src="<?php echo $view['assets']->getUrl('js/chosen.jquery.js') ?>"></script>
         <script type="text/javascript" src="<?php echo $view['assets']->getUrl('js/jquery.mCustomScrollbar.concat.min.js') ?>"></script>
         <script type="text/javascript" src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-       <?php if(ga_tracking){ ?>
+       <?php if($view->container->parameters['ga_tracking']){ ?>
             <script>
                 (function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function(){
                 (i[r].q = i[r].q || []).push(arguments)}, i[r].l = 1 * new Date(); a = s.createElement(o),
                         m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
                 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-                        ga('create', <?php echo ga_tracking; ?>, 'auto');
+                        ga('create', <?php echo $view->container->parameters['ga_tracking']; ?>, 'auto');
                         ga('send', 'pageview');
             </script>
         <?php } ?>
